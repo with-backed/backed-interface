54b91ee059c8305da1beea17688ba203f265284c
Christopher Nascone <chris@nasc.one>
2021-10-16 11:38:12 -0400

    introduce some top-level layouts

---
+++ stories/ThreeColumn.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { GridItem } from './helpers';
+import ThreeColumn from '../components/layouts/ThreeColumn';
+
+export default {
+  title: 'layouts/ThreeColumn',
+  component: ThreeColumn,
+};
+
+export const LayoutEmpty = () => <ThreeColumn />;
+
+export const LayoutThreeItems = () => (
+  <ThreeColumn>
+    {[1, 2, 3].map((index) => (
+      <GridItem key={index}>{index}</GridItem>
+    ))}
+  </ThreeColumn>
+);
+
+export const LayoutFiveItems = () => (
+  <ThreeColumn>
+    {[1, 2, 3, 4, 5].map((index) => (
+      <GridItem key={index}>{index}</GridItem>
+    ))}
+  </ThreeColumn>
+);
