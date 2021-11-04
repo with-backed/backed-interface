784891c094122e69a4483917cd6747daf286c54f
Wilson Cusack <wilson.cusack@coinbase.com>
2021-11-04 16:37:45 -0400

    added homepage link to /test, when env is rinkeby

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,16 @@ import React from 'react';
 export default function Home() {
   return (
     <div>
-      Welcome! Homepage in progress, try
-      <Link href="/loans/create">Creating a loan</Link>
+      <p>
+      Welcome! Homepage in progress, try <Link href="/loans/create"> Creating a loan</Link>
+      </p>
+      <p>
+      {
+        process.env.NEXT_PUBLIC_ENV == 'local' ? 
+        <Link href="/test">Get Rinkeby DAI and an NFT!</Link>
+        : ''
+      }
+      </p>
     </div>
   );
 }
