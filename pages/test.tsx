9186045ac1a4cc2d248ad96ac8216858e934402f
Christopher Nascone <cnasc@users.noreply.github.com>
2021-11-05 12:49:40 -0400

    Test page uses new layout (#29)

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,56 +1,79 @@
+import { GetServerSideProps } from 'next';
 import dynamic from 'next/dynamic';
 import React, { useState } from 'react';
 import { ethers } from 'ethers';
 import { PawnShopHeader } from 'components/PawnShopHeader';
 import { MockDAI__factory, MockPUNK__factory } from 'abis/types';
 import TransactionButton from 'components/ticketPage/TransactionButton';
+import { PageWrapper } from 'components/layouts/PageWrapper';
+import { Fieldset } from 'components/Fieldset';
+import { ThreeColumn } from 'components/layouts/ThreeColumn';
 
 const ConnectWallet = dynamic(
   () => import('components/ConnectWallet').then(mod => mod.ConnectWallet),
   { ssr: false }
 );
 
-export default function Test() {
+type TestProps = {
+  mockDAIContract: string;
+  mockPunkContract: string;
+}
+
+export const getServerSideProps: GetServerSideProps<TestProps> = async () => {
+  const mockDAIContract = process.env.NEXT_PUBLIC_MOCK_DAI_CONTRACT;
+  const mockPunkContract = process.env.NEXT_PUBLIC_MOCK_PUNK_CONTRACT;
+
+  return {
+    props: {
+      mockDAIContract,
+      mockPunkContract,
+    }
+  }
+}
+
+export default function Test({ mockDAIContract, mockPunkContract }: TestProps) {
   const [account, setAccount] = useState(null);
 
   return (
-    <div id="ticket-page-wrapper">
+    <PageWrapper>
       <PawnShopHeader
         account={account}
         setAccount={setAccount}
         message="get an NFT and DAI"
       />
-
-      <fieldset className="standard-fieldset float-left">
-        <legend> mint an NFT </legend>
-        {account == null ? (
-          <ConnectWallet
-            account={account}
-            addressSetCallback={setAccount}
-          />
-        )
-          : (
-            <MintPunk account={account} />
-          )}
-      </fieldset>
-
-      <fieldset id="create-explainer-fieldset" className="standard-fieldset float-left">
-        <legend> mint DAI </legend>
-        {account == null ? (
-          <ConnectWallet
-            account={account}
-            addressSetCallback={setAccount}
-          />
-        )
-          : (
-            <MintDAI account={account} />
-          )}
-      </fieldset>
-    </div>
+      <ThreeColumn>
+        <Fieldset legend="mint an NFT">
+          {account == null ? (
+            <ConnectWallet
+              account={account}
+              addressSetCallback={setAccount}
+            />
+          )
+            : (
+              <MintPunk account={account} mockPunkContract={mockPunkContract} />
+            )}
+        </Fieldset>
+        <Fieldset legend="mint DAI">
+          {account == null ? (
+            <ConnectWallet
+              account={account}
+              addressSetCallback={setAccount}
+            />
+          )
+            : (
+              <MintDAI account={account} mockDAIContract={mockDAIContract} />
+            )}
+        </Fieldset>
+      </ThreeColumn>
+    </PageWrapper>
   );
 }
 
-function MintPunk({ account }: { account: string }) {
+type MintPunkProps = {
+  account: string;
+  mockPunkContract: string;
+};
+function MintPunk({ account, mockPunkContract }: MintPunkProps) {
   const [txHash, setTxHash] = useState('');
   const [txPending, setTxPending] = useState(false);
   const [id, setId] = useState<ethers.BigNumber>(null);
@@ -58,7 +81,7 @@ function MintPunk({ account }: { account: string }) {
   const mintPunk = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner(0);
-    const punk = MockPUNK__factory.connect(process.env.NEXT_PUBLIC_MOCK_PUNK_CONTRACT, signer);
+    const punk = MockPUNK__factory.connect(mockPunkContract, signer);
     const t = await punk.mint();
     setTxHash(t.hash);
     setTxPending(true);
@@ -76,7 +99,7 @@ function MintPunk({ account }: { account: string }) {
     const provider = new ethers.providers.JsonRpcProvider(
       process.env.NEXT_PUBLIC_JSON_RPC_PROVIDER,
     );
-    const punk = MockPUNK__factory.connect(process.env.NEXT_PUBLIC_MOCK_PUNK_CONTRACT, provider);
+    const punk = MockPUNK__factory.connect(mockPunkContract, provider);
     const filter = punk.filters.Transfer(
       null,
       account,
@@ -98,22 +121,26 @@ function MintPunk({ account }: { account: string }) {
         isPending={txPending}
       />
       <p>
-        {`NFT contract address ${process.env.NEXT_PUBLIC_MOCK_PUNK_CONTRACT} (you will 
-        need this when creating a loan)`}
+        NFT contract address {mockPunkContract} (you will
+        need this when creating a loan)
       </p>
-      {id == null ? '' : <p>{`Minted token ID ${id.toString()}`}</p>}
+      {Boolean(id) && <p>Minted token ID {id.toString()}</p>}
     </div>
   );
 }
 
-function MintDAI({ account }: { account: string }) {
+type MintDAIProps = {
+  account: string;
+  mockDAIContract: string;
+}
+function MintDAI({ account, mockDAIContract }: MintDAIProps) {
   const [txHash, setTxHash] = useState('');
   const [txPending, setTxPending] = useState(false);
 
   const mint = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner(0);
-    const dai = MockDAI__factory.connect(process.env.NEXT_PUBLIC_MOCK_DAI_CONTRACT, signer);
+    const dai = MockDAI__factory.connect(mockDAIContract, signer);
     const t = await dai.mint(ethers.BigNumber.from(10000), account);
     setTxHash(t.hash);
     setTxPending(true);
@@ -131,7 +158,7 @@ function MintDAI({ account }: { account: string }) {
     const provider = new ethers.providers.JsonRpcProvider(
       process.env.NEXT_PUBLIC_JSON_RPC_PROVIDER,
     );
-    const dai = MockDAI__factory.connect(process.env.NEXT_PUBLIC_MOCK_DAI_CONTRACT, provider);
+    const dai = MockDAI__factory.connect(mockDAIContract, provider);
     const filter = dai.filters.Transfer(
       null,
       account,
@@ -151,8 +178,8 @@ function MintDAI({ account }: { account: string }) {
         isPending={txPending}
       />
       <p>
-        {`DAI contract address ${process.env.NEXT_PUBLIC_MOCK_DAI_CONTRACT} (you will 
-        need this when creating a loan)`}
+        DAI contract address {mockDAIContract} (you will
+        need this when creating a loan)
       </p>
     </div>
   );
