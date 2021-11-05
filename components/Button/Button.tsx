07433d196e68de9a9052903e3dc9736d12b4f564
Christopher Nascone <chris@nasc.one>
2021-11-05 11:23:04 -0400

    Add Button component

---
+++ components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React, { ButtonHTMLAttributes } from 'react';
+import styles from './Button.module.css';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> { };
+export function Button({ children, ...props }: ButtonProps) {
+  return (
+    <button className={styles.button} {...props}>
+      {children}
+    </button>
+  );
+}
