53bea201e29f1e6001ed2487466e11539e526b66
Christopher Nascone <chris@nasc.one>
2021-11-08 12:28:37 -0500

    Add Fieldset story

---
+++ stories/Fieldset.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Fieldset } from 'components/Fieldset';
+import { Input } from 'components/Input';
+import { Button } from 'components/Button';
+import { FormWrapper } from 'components/layouts/FormWrapper';
+
+export default {
+  title: 'components/Fieldset',
+  component: Fieldset,
+}
+
+export const FieldsetStyles = () => {
+  return (
+    <Fieldset legend="Storybook Fieldset">
+      <FormWrapper>
+        <Input title="an input" onChange={() => { }} />
+        <Input title="another input" onChange={() => { }} />
+        <Button>Don't do anything</Button>
+      </FormWrapper>
+    </Fieldset>
+  );
+}
