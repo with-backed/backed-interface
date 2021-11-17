7482bf5f2da1695219ff034a610544ac5a741022
Christopher Nascone <chris@nasc.one>
2021-11-17 16:46:22 -0500

    Introducing LoanCard component

---
+++ components/LoanCard/LoanCard.tsx
@@ -0,0 +1,85 @@
+import { ethers } from 'ethers';
+import { formattedAnnualRate } from 'lib/interest';
+import Link from 'next/link';
+import React, { FunctionComponent, useMemo } from 'react';
+import styles from './LoanCard.module.css';
+import { useTokenMetadata } from 'hooks/useTokenMetadata';
+import { Media } from 'components/Media';
+
+const Attributes: FunctionComponent = ({ children }) => {
+  return <div className={styles.attributes}>{children}</div>;
+};
+
+type Loan = {
+  id: string;
+  loanAssetSymbol: string;
+  loanAssetDecimal: number;
+  loanAmount: string;
+  perSecondInterestRate: string;
+  collateralTokenURI: string;
+  collateralTokenID: string;
+};
+
+type LoanCardProps = {
+  loan: Loan;
+};
+
+export function LoanCard({
+  loan: {
+    id,
+    loanAmount,
+    loanAssetDecimal,
+    loanAssetSymbol,
+    perSecondInterestRate,
+    collateralTokenURI,
+    collateralTokenID,
+  },
+}: LoanCardProps) {
+  const title = `View loan #${id}`;
+  const formattedLoanAmount = useMemo(
+    () =>
+      `${ethers.utils.formatUnits(
+        loanAmount,
+        loanAssetDecimal,
+      )} ${loanAssetSymbol}`,
+    [loanAmount, loanAssetDecimal, loanAssetSymbol],
+  );
+  const maybeMetadata = useTokenMetadata({
+    tokenURI: collateralTokenURI,
+    tokenID: ethers.BigNumber.from(collateralTokenID),
+  });
+
+  if (maybeMetadata.isLoading) {
+    // TODO: @cnasc loading state
+    return null;
+  } else if (!maybeMetadata.metadata) {
+    // TODO: @cnasc failed to fetch state
+  } else {
+    const { mediaUrl, mediaMimeType, name } = maybeMetadata.metadata;
+    return (
+      <Link href={`/loans/${id}`}>
+        <a className={styles.link} aria-label={title} title={title}>
+          <div className={styles.card}>
+            <Media
+              media={mediaUrl}
+              mediaMimeType={mediaMimeType}
+              autoPlay={false}
+            />
+            <span>{name}</span>
+            <Attributes>
+              <span>{formattedLoanAmount}</span>
+              <span>
+                {formattedAnnualRate(
+                  ethers.BigNumber.from(perSecondInterestRate),
+                )}
+                % interest
+              </span>
+            </Attributes>
+          </div>
+        </a>
+      </Link>
+    );
+  }
+
+  return null;
+}
