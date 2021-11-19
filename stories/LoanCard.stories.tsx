02f874260d93d7c16fc99c502ce63ef1fb420ea0
Christopher Nascone <chris@nasc.one>
2021-11-19 14:13:53 -0500

    Story cleanup

--- stories/LoanCard.stories.tsx
+++ stories/LoanCard.stories.tsx
@@ -9,28 +9,6 @@ export default {
   component: LoanCard,
 };
 
-const loans = [
-  {
-    collateralTokenURI:
-      'ipfs://QmXuEFJVjQrHX7GRWY2WnbUP59re3WsyDLZoKqXvRPSxBY/6',
-    id: '18',
-    loanAmount: '93000000000000000000',
-    loanAssetDecimal: 18,
-    loanAssetSymbol: 'DAI',
-    perSecondInterestRate: '9',
-    collateralTokenID: '0',
-  },
-  {
-    collateralTokenURI: 'https://wrappedpunks.com:3000/api/punks/metadata/1003',
-    id: '2',
-    loanAmount: '6969000000000000000000',
-    loanAssetDecimal: 18,
-    loanAssetSymbol: 'DAI',
-    perSecondInterestRate: '21',
-    collateralTokenID: '7',
-  },
-];
-
 export const LoanCards = () => (
   <FiveColumn>
     <LoanCardLoading />
