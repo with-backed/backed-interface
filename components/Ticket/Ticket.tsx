5ed9153f4d49f27a9cbad65a13954932711474b4
Christopher Nascone <cnasc@users.noreply.github.com>
2021-11-19 15:02:21 -0500

    chore: remove semantic-ui from project (#94)
    * chore: remove semantic-ui from project
    
    * css adjustment
    
    * fix button font size regression

--- components/Ticket/Ticket.tsx
+++ components/Ticket/Ticket.tsx
@@ -1,10 +1,4 @@
-import React, {
-  FunctionComponent,
-  useCallback,
-  useEffect,
-  useState,
-} from 'react';
-import { Dimmer, Loader } from 'semantic-ui-react';
+import React, { FunctionComponent, useCallback, useState } from 'react';
 import { getLoanInfo } from 'lib/loan';
 import TicketPageBody from 'components/ticketPage/TicketPageBody';
 import { PawnShopHeader } from 'components/PawnShopHeader';
@@ -27,13 +21,7 @@ export const Loan: FunctionComponent<LoanProps> = ({ serverLoanInfo }) => {
   return (
     <PageWrapper>
       <PawnShopHeader message={`pawn loan #${loanInfo.loanId}`} />
-      {loanInfo == null ? (
-        <Dimmer active={loanInfo == null} inverted>
-          <Loader inverted content="Loading" />
-        </Dimmer>
-      ) : (
-        <TicketPageBody loanInfo={loanInfo} refresh={fetchData} />
-      )}
+      <TicketPageBody loanInfo={loanInfo} refresh={fetchData} />
     </PageWrapper>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import 'semantic-ui-css/semantic.min.css';
 import '../styles/global.css';
 import '../styles/ticketPage.css';
 import '../styles/collateralCard.css';
