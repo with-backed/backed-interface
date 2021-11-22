57ce4a6385b9dd26cb47e75292d284dcd75ca1b5
Christopher Nascone <cnasc@users.noreply.github.com>
2021-11-22 17:03:02 -0500

    feat: we have a homepage now (#107)
    * rudimentary homepage
    
    * limit: 20
    
    * simplify
    
    * sort createdAtTimestamp desc

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,66 @@
+import { FiveColumn } from 'components/layouts/FiveColumn';
+import { PageWrapper } from 'components/layouts/PageWrapper';
+import { LoanCard, Loan } from 'components/LoanCard';
+import { PawnShopHeader } from 'components/PawnShopHeader';
+import { nftBackedLoansClient } from 'lib/urql';
+import { GetServerSideProps } from 'next';
 import Link from 'next/link';
 import React from 'react';
 
-export default function Home() {
+const homepageQuery = `
+query {
+  loans(where: { closed: false}, first: 20, orderBy: createdAtTimestamp, orderDirection: desc) {
+    collateralTokenId
+    collateralTokenURI
+    id
+    loanAmount
+    loanAssetSymbol
+    loanAssetDecimal
+    perSecondInterestRate
+  }
+}
+`;
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  try {
+    const {
+      data: { loans },
+    } = await nftBackedLoansClient.query(homepageQuery).toPromise();
+
+    return {
+      props: {
+        loans: loans as Loan[],
+      },
+    };
+  } catch (e) {
+    // TODO: log this error somewhere
+    return {
+      props: {
+        loans: [],
+      },
+    };
+  }
+};
+
+type HomeProps = {
+  loans: Loan[];
+};
+export default function Home({ loans }: HomeProps) {
   return (
-    <div>
+    <PageWrapper>
+      <PawnShopHeader message="available for lending" />
+      <FiveColumn>
+        {loans.map((loan) => (
+          <LoanCard key={loan.id} loan={loan} />
+        ))}
+      </FiveColumn>
       <p>
         Welcome! Homepage in progress, try{' '}
         <Link href="/loans/create"> Creating a loan</Link>
       </p>
-      <p>
-        {process.env.NEXT_PUBLIC_ENV == 'rinkeby' ? (
-          <Link href="/test">Get Rinkeby DAI and an NFT!</Link>
-        ) : (
-          ''
-        )}
-      </p>
-    </div>
+      {process.env.NEXT_PUBLIC_ENV === 'rinkeby' && (
+        <Link href="/test">Get Rinkeby DAI and an NFT!</Link>
+      )}
+    </PageWrapper>
   );
 }
