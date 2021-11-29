811e89b040543f8f615854c01432d0a81c75e69a
Christopher Nascone <cnasc@users.noreply.github.com>
2021-11-29 14:10:38 -0500

    feat: marquee stops scrolling on click (#128)

--- components/Marquee/Marquee.tsx
+++ components/Marquee/Marquee.tsx
@@ -1,4 +1,9 @@
-import React, { FunctionComponent, useMemo } from 'react';
+import React, {
+  FunctionComponent,
+  useCallback,
+  useMemo,
+  useState,
+} from 'react';
 import styles from './Marquee.module.css';
 
 export const MarqueeSpacer = () => {
@@ -6,16 +11,24 @@ export const MarqueeSpacer = () => {
 };
 
 export const Marquee: FunctionComponent = ({ children }) => {
+  const [isPaused, setIsPaused] = useState(false);
+  const className = useMemo(
+    () => `${styles.scrolling} ${isPaused ? styles.paused : ''}`,
+    [isPaused],
+  );
+  const handleClick = useCallback(() => {
+    setIsPaused((state) => !state);
+  }, [setIsPaused]);
   return (
-    <div className={styles.container}>
-      <div className={styles.scrolling}>{children}</div>
-      <div className={styles.scrolling} aria-hidden={true}>
+    <div className={styles.container} onClick={handleClick}>
+      <div className={className}>{children}</div>
+      <div className={className} aria-hidden={true}>
         {children}
       </div>
-      <div className={styles.scrolling} aria-hidden={true}>
+      <div className={className} aria-hidden={true}>
         {children}
       </div>
-      <div className={styles.scrolling} aria-hidden={true}>
+      <div className={className} aria-hidden={true}>
         {children}
       </div>
     </div>
