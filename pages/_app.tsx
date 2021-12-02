3ae828263a16e3a06ad1a51552cc116425e1a21c
Christopher Nascone <chris@nasc.one>
2021-12-02 10:56:52 -0500

    add and configure web3-react

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,22 @@ import '../styles/global.css';
 import 'normalize.css';
 import { AppProps } from 'next/app';
 import { AccountProvider } from 'context/account';
+import { Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
+
+const getLibrary: React.ComponentProps<typeof Web3ReactProvider>['getLibrary'] =
+  (provider) => {
+    const library = new Web3Provider(provider);
+    library.pollingInterval = 8000;
+    return library;
+  };
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AccountProvider>
-      <Component {...pageProps} />
-    </AccountProvider>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <AccountProvider>
+        <Component {...pageProps} />
+      </AccountProvider>
+    </Web3ReactProvider>
   );
 }
