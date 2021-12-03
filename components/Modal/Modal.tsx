b7d81eec6285accc8cb0d13c906be19835ab2c19
Christopher Nascone <chris@nasc.one>
2021-12-03 11:09:13 -0500

    modal changes to support narrower width

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -5,14 +5,16 @@ import React, { FunctionComponent } from 'react';
 type ModalProps = {
   dialog: DialogStateReturn;
   heading?: string;
+  width?: 'regular' | 'narrow';
 };
 export const Modal: FunctionComponent<ModalProps> = ({
   children,
   dialog,
   heading,
+  width = 'regular',
 }) => {
   return (
-    <Dialog className={styles.dialog} {...dialog}>
+    <Dialog className={`${styles.dialog} ${styles[width]}`} {...dialog}>
       {Boolean(heading) && <h3 className={styles.heading}>{heading}</h3>}
       <div className={styles['scroll-box']}>{children}</div>
     </Dialog>
