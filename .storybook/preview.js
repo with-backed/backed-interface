30fab75375ae7666274b990f5f36e8ad86f87772
Christopher Nascone <chris@nasc.one>
2021-12-03 11:00:17 -0500

    add storybook config for Next/Image

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,14 @@
 import '../styles/global.css';
 import 'normalize.css';
+import * as NextImage from 'next/image';
+
+const OriginalNextImage = NextImage.default;
+
+// If we don't do this, Storybook will break when rendering things that use Next/Image
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
