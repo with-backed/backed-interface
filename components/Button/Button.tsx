555b36979226c08b69eedfc1647290206e81dbec
Christopher Nascone <chris@nasc.one>
2021-12-03 11:05:34 -0500

    introducing WalletButton

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,7 +2,7 @@ import React, { ButtonHTMLAttributes, ComponentProps } from 'react';
 import { DialogDisclosure } from 'reakit/Dialog';
 import styles from './Button.module.css';
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 export function Button({ children, ...props }: ButtonProps) {
   return (
     <button className={styles.button} {...props}>
