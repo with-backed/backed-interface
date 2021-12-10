f9f531469dd04d280901f36345d5ccff27b609e1
Christopher Nascone <chris@nasc.one>
2021-12-10 13:43:53 -0500

    Handle not found loans with new method

--- pages/loans/[id].tsx
+++ pages/loans/[id].tsx
@@ -1,44 +1,13 @@
 import { GetServerSideProps } from 'next';
 import { Loan } from 'components/Ticket';
 import { LoanInfo } from 'lib/LoanInfoType';
-import { getLoanInfo } from 'lib/loan';
-import { nftBackedLoansClient } from 'lib/urql';
+
 import { ethers } from 'ethers';
 import { useMemo } from 'react';
-
-const loanPageQuery = `
-query ($id: ID!) {
-  loan(id: $id) {
-    id
-    loanAssetContractAddress
-    collateralContractAddress
-    collateralTokenId
-    perSecondInterestRate
-    accumulatedInterest
-    lastAccumulatedTimestamp
-    durationSeconds
-    loanAmount
-    closed
-    loanAssetDecimal
-    loanAssetSymbol
-    lendTicketHolder
-    borrowTicketHolder
-  }
-}
-`;
-
-const bigNumberProperties = [
-  'loanId',
-  'collateralTokenId',
-  'perSecondInterestRate',
-  'accumulatedInterest',
-  'lastAccumulatedTimestamp',
-  'durationSeconds',
-  'loanAmount',
-];
+import { getLoanInfoGraphQL } from 'lib/loan';
 
 export type LoanPageProps = {
-  loanInfoJson: string | null;
+  loanInfoJson: string;
   id: string;
 };
 
@@ -46,20 +15,14 @@ export const getServerSideProps: GetServerSideProps<LoanPageProps> = async (
   context,
 ) => {
   const id = context.params?.id as string;
-  const {
-    data: { loan },
-  } = await nftBackedLoansClient.query(loanPageQuery, { id }).toPromise();
-  loan.interestOwed = ethers.BigNumber.from('0');
-  loan.loanId = loan.id;
-  bigNumberProperties.forEach((prop) => {
-    if (loan) {
-      if (loan[prop]) {
-        loan[prop] = ethers.BigNumber.from(loan[prop]);
-      }
-    }
-  });
-  loan.lender = loan.lendTicketHolder;
-  loan.borrower = loan.borrowTicketHolder;
+  const loan = await getLoanInfoGraphQL(id);
+
+  // The Graph didn't have loan, and fallback call errored.
+  if (!loan) {
+    return {
+      notFound: true,
+    };
+  }
 
   const loanInfoJson = JSON.stringify(loan);
   return {
@@ -72,7 +35,7 @@ export const getServerSideProps: GetServerSideProps<LoanPageProps> = async (
 
 export default function Loans({ loanInfoJson, id }: LoanPageProps) {
   const loanInfo = useMemo(
-    () => (loanInfoJson ? parseLoanInfoJson(loanInfoJson) : null),
+    () => parseLoanInfoJson(loanInfoJson),
     [loanInfoJson],
   );
 
