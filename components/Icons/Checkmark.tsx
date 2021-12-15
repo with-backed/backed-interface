8abeb59af81f0ae255237096a5bfa559ae6e7b95
Christopher Nascone <cnasc@users.noreply.github.com>
2021-12-15 11:22:42 -0500

    feat: update CompletedButton with Checkmark (#192)

---
+++ components/Icons/Checkmark.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Checkmark = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={7} height={10} fill="none">
+    <path
+      stroke="#75187C"
+      strokeLinecap="round"
+      strokeWidth={2}
+      d="m1 5.469 1.004 2.008c.386.772 1.503.725 1.823-.076L6 1.969"
+    />
+  </svg>
+);
