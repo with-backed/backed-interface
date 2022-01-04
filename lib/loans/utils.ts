b03963bdd0a436385a0a9515da516f79e95aa42f
Wilson Cusack <wilson.cusack@coinbase.com>
2022-01-04 15:52:29 -0500

    use seconds rather than miliseconds

--- lib/loans/utils.ts
+++ lib/loans/utils.ts
@@ -12,12 +12,12 @@ export function parseSubgraphLoan(loan: SubgraphLoan): Loan {
   const lastAccumulatedTimestamp = ethers.BigNumber.from(
     loan.lastAccumulatedTimestamp,
   );
-  const now = ethers.BigNumber.from(Date.now());
+  const now = ethers.BigNumber.from(Date.now()).div(1000);
   let interestOwed = ethers.BigNumber.from(0);
   if (!lastAccumulatedTimestamp.eq(0)) {
     interestOwed = loanAmount
-      .mul(perSecondInterestRate)
       .mul(now.sub(lastAccumulatedTimestamp))
+      .mul(perSecondInterestRate)
       .div(SCALAR)
       .add(accumulatedInterest);
   }
