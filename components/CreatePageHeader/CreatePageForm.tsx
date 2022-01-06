49dc93ce8456b37da33dc564e5daf3b3d23683aa
Christopher Nascone <cnasc@users.noreply.github.com>
2022-01-06 13:16:33 -0500

    feat: can now create a loan with new create page (#255)
    * fix: button completed icon css
    
    * fix: NFTMedia uses fallback
    
    * chore: refactor and initial CreatePageForm
    
    * feat: can now create a loan with new create page

---
+++ components/CreatePageHeader/CreatePageForm.tsx
@@ -0,0 +1,161 @@
+import { Button, CompletedButton, TransactionButton } from 'components/Button';
+import { ethers } from 'ethers';
+import { ErrorMessage, Field, Formik } from 'formik';
+import { useWeb3 } from 'hooks/useWeb3';
+import {
+  jsonRpcERC20Contract,
+  jsonRpcLoanFacilitator,
+  web3LoanFacilitator,
+} from 'lib/contracts';
+import { getLoanAssets, LoanAsset } from 'lib/loanAssets';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import * as Yup from 'yup';
+import styles from './CreatePageHeader.module.css';
+import { State } from './State';
+
+const SECONDS_IN_A_DAY = 60 * 60 * 24;
+const SECONDS_IN_A_YEAR = 31_536_000;
+const INTEREST_RATE_PERCENT_DECIMALS = 8;
+const MIN_RATE = 1 / 10 ** INTEREST_RATE_PERCENT_DECIMALS;
+
+type CreatePageFormProps = {
+  collateralAddress: string;
+  collateralTokenID: ethers.BigNumber;
+  state: State;
+};
+
+export function CreatePageForm({
+  collateralAddress,
+  collateralTokenID,
+  state,
+}: CreatePageFormProps) {
+  const { account } = useWeb3();
+  const buttonText = useMemo(() => 'Mint Borrower Ticket', []);
+  const [txHash, setTxHash] = useState('');
+  const [waitingForTx, setWaitingForTx] = useState(false);
+  const [loanAssetOptions, setLoanAssetOptions] = useState<LoanAsset[]>([]);
+
+  const wait = useCallback(async () => {
+    const contract = jsonRpcLoanFacilitator();
+    const filter = contract.filters.CreateLoan(null, account, null, null, null);
+    contract.once(filter, (id) => {
+      setWaitingForTx(false);
+      window.location.assign(`/loans/${id.toString()}`);
+    });
+  }, [account]);
+
+  const mint = useCallback(
+    async ({
+      loanAssetContractAddress,
+      loanAmount,
+      interestRate,
+      duration,
+    }) => {
+      const assetContract = jsonRpcERC20Contract(loanAssetContractAddress);
+      const loanAssetDecimals = await assetContract.decimals();
+      const durationInSeconds = Math.ceil(duration * SECONDS_IN_A_DAY);
+      const interestRatePerSecond = ethers.BigNumber.from(
+        Math.floor(interestRate * 10 ** INTEREST_RATE_PERCENT_DECIMALS),
+      ).div(SECONDS_IN_A_YEAR);
+
+      const contract = web3LoanFacilitator();
+      const t = await contract.createLoan(
+        collateralTokenID,
+        collateralAddress,
+        interestRatePerSecond,
+        ethers.utils.parseUnits(loanAmount.toString(), loanAssetDecimals),
+        loanAssetContractAddress,
+        durationInSeconds,
+        // If they've gotten this far, they must have an account.
+        account!,
+      );
+      setTxHash(t.hash);
+      setWaitingForTx(true);
+      t.wait()
+        .then(() => {
+          wait();
+          setWaitingForTx(true);
+        })
+        .catch((err) => {
+          setWaitingForTx(false);
+          console.error(err);
+        });
+    },
+    [account, collateralAddress, collateralTokenID, wait],
+  );
+
+  const loadAssets = useCallback(async () => {
+    const assets = await getLoanAssets();
+    setLoanAssetOptions(assets);
+  }, []);
+
+  useEffect(() => {
+    loadAssets();
+  }, [loadAssets]);
+
+  if (state < State.Form) {
+    return <Button disabled>{buttonText}</Button>;
+  }
+
+  return (
+    <Formik
+      initialValues={{
+        loanAssetContractAddress:
+          loanAssetOptions.length > 0 ? loanAssetOptions[0].address : '',
+        loanAmount: 0,
+        interestRate: 0,
+        duration: 0,
+      }}
+      validationSchema={Yup.object({
+        loanAssetContractAddress: Yup.string(),
+        loanAmount: Yup.number().moreThan(0),
+        interestRate: Yup.number().min(MIN_RATE),
+        duration: Yup.number().moreThan(0),
+      })}
+      isInitialValid={false}
+      onSubmit={mint}>
+      {(formik) => (
+        <form className={styles.form} onSubmit={formik.handleSubmit}>
+          <label htmlFor="loanAssetContractAddress">
+            <span>Loan Denomination</span>
+            <Field name="loanAssetContractAddress" as="select">
+              {loanAssetOptions.map(({ symbol, address }) => {
+                return (
+                  <option value={address} key={address}>
+                    {symbol}
+                  </option>
+                );
+              })}
+            </Field>
+          </label>
+
+          <label htmlFor="loanAmount">
+            <span>Minimum Loan Amount</span>
+            <Field name="loanAmount" type="number" placeholder="0" />
+          </label>
+          <ErrorMessage name="loanAmount" />
+
+          <label htmlFor="interestRate">
+            <span>Maximum Interest Rate</span>
+            <Field name="interestRate" type="number" placeholder={`0 %`} />
+          </label>
+          <ErrorMessage name="interestRate" />
+
+          <label htmlFor="duration">
+            <span>Minimum Duration</span>
+            <Field name="duration" type="number" placeholder="0 Days" />
+          </label>
+          <ErrorMessage name="loanAmount" />
+
+          <TransactionButton
+            text={buttonText}
+            type="submit"
+            txHash={txHash}
+            isPending={waitingForTx}
+            disabled={!formik.isValid}
+          />
+        </form>
+      )}
+    </Formik>
+  );
+}
--- pages/loans/create.tsx
+++ pages/loans/create.tsx
@@ -20,6 +20,7 @@ import { DialogDisclosureButton } from 'components/Button';
 import { FormWrapper } from 'components/layouts/FormWrapper';
 import { useWeb3 } from 'hooks/useWeb3';
 import { PageWrapper } from 'components/layouts/PageWrapper';
+import { CreatePageHeader } from 'components/CreatePageHeader';
 
 export default function Create() {
   const { account } = useWeb3();
@@ -32,68 +33,5 @@ export default function Create() {
     return [getNftContractAddress(selectedNFT), selectedNFT?.identifier];
   }, [selectedNFT]);
 
-  return (
-    <PageWrapper>
-      <ThreeColumn>
-        <Fieldset legend="set loan terms">
-          {Boolean(account) ? (
-            <div>
-              <p className={styles.mintBorrowExplainer}>
-                After selecting an NFT and setting the loan terms, mint the
-                borrower ticket to lock up your NFT and make your loan available
-                to lenders.
-              </p>
-              <CreateTicketForm
-                collateralAddress={collateralAddress}
-                collateralTokenID={collateralTokenID}
-                isCollateralApproved={
-                  !!(
-                    isCollateralApproved ||
-                    (selectedNFT && isNFTApprovedForCollateral(selectedNFT))
-                  )
-                }
-              />
-            </div>
-          ) : (
-            <ConnectWallet />
-          )}
-        </Fieldset>
-        <Fieldset legend="select collateral NFT">
-          {collateralAddress === '' && (
-            <div className={styles.collateralExplainer}>
-              <p>
-                This NFT will be locked up as collateral for your loan. If you
-                fail to repay the loan, the NFT will be transferred to the
-                lender.
-              </p>
-            </div>
-          )}
-          {selectedNFT !== undefined && (
-            <FormWrapper>
-              <AuthorizedNFT
-                nft={selectedNFT}
-                handleApproved={() => setIsCollateralApproved(true)}
-              />
-              <DialogDisclosureButton {...dialog}>
-                Or select a different NFT
-              </DialogDisclosureButton>
-            </FormWrapper>
-          )}
-          <Provider value={eip721Client}>
-            <NFTCollateralPicker
-              hiddenNFTAddresses={HIDDEN_NFT_ADDRESSES}
-              connectedWallet={account || ''}
-              handleSetSelectedNFT={setSelectedNFT}
-              dialog={dialog}
-            />
-          </Provider>
-          {!collateralAddress && (
-            <DialogDisclosureButton {...dialog}>
-              Select an NFT
-            </DialogDisclosureButton>
-          )}
-        </Fieldset>
-      </ThreeColumn>
-    </PageWrapper>
-  );
+  return <CreatePageHeader />;
 }
