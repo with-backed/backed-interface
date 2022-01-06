9ca9fbaf224865eac6e11f4e155e491ac5012606
Christopher Nascone <chris@nasc.one>
2022-01-06 10:48:24 -0500

    feat: inputs match design

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,69 +1,14 @@
-import React, {
-  ChangeEvent,
-  FunctionComponent,
-  InputHTMLAttributes,
-  useCallback,
-  useMemo,
-  useRef,
-} from 'react';
-import debounce from 'lodash/debounce';
+import React, { FunctionComponent, InputHTMLAttributes } from 'react';
+
 import styles from './Input.module.css';
 
-// Carried over from earlier implementation; we can play with this value and
-// see what provides the best feel.
-const WAIT_DURATION_IN_MILLISECONDS = 500;
-
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  error?: React.ReactNode;
-  message?: React.ReactNode;
+  color?: 'light' | 'dark';
 }
 
 export const Input: FunctionComponent<InputProps> = ({
-  onChange,
-  error,
-  message,
-  title,
+  color = 'light',
   ...props
 }) => {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const handleWheel = useCallback(() => {
-    if (!inputRef.current) {
-      return;
-    }
-    // this stops the scroll event from changing the input (annoying)
-    inputRef.current.blur();
-    // this refocuses the input briefly after scrolling to prevent a different annoyance
-    setTimeout(() => {
-      inputRef.current?.focus();
-    });
-  }, []);
-
-  const debouncedHandleChange = useMemo(
-    () =>
-      debounce((event: ChangeEvent<HTMLInputElement>) => {
-        onChange(event);
-      }, WAIT_DURATION_IN_MILLISECONDS),
-    [onChange],
-  );
-
-  const hasError = Boolean(error);
-  const hasMessage = Boolean(message);
-  return (
-    <div className={styles.wrapper}>
-      <label className={hasError ? styles.errorLabel : styles.label}>
-        {title}
-        <input
-          aria-invalid={hasError}
-          className={hasError ? styles.errorInput : styles.input}
-          onChange={debouncedHandleChange}
-          ref={inputRef}
-          onWheel={handleWheel}
-          {...props}
-        />
-      </label>
-      {hasError && <div className={styles.errorMessage}>{error}</div>}
-      {hasMessage && message}
-    </div>
-  );
+  return <input className={styles[color]} {...props} />;
 };
