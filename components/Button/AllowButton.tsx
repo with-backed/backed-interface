b02e1daae04dc1f3737da6c6eee35bcf6bf14925
Christopher Nascone <cnasc@users.noreply.github.com>
2022-01-27 17:10:15 -0500

    test: boost coverage on Button components (#340)

--- components/Button/AllowButton.tsx
+++ components/Button/AllowButton.tsx
@@ -1,9 +1,7 @@
 import { useCallback, useMemo, useState } from 'react';
-import { ethers } from 'ethers';
 import { TransactionButton } from 'components/Button';
-import { web3Erc20Contract } from 'lib/contracts';
-import { useWeb3 } from 'hooks/useWeb3';
 import { CompletedButton } from 'components/Button';
+import { authorizeCurrency } from 'lib/authorizations/authorizeCurrency';
 
 interface AllowButtonProps {
   contractAddress: string;
@@ -18,28 +16,17 @@ export function AllowButton({
   callback,
   done,
 }: AllowButtonProps) {
-  const { account } = useWeb3();
   const [txHash, setTxHash] = useState('');
   const [waitingForTx, setWaitingForTx] = useState(false);
 
-  const allow = useCallback(async () => {
-    const contract = web3Erc20Contract(contractAddress);
-    const t = await contract.approve(
-      process.env.NEXT_PUBLIC_NFT_LOAN_FACILITATOR_CONTRACT || '',
-      ethers.BigNumber.from(2).pow(256).sub(1),
-    );
-    setWaitingForTx(true);
-    setTxHash(t.hash);
-    t.wait()
-      .then(() => {
-        callback();
-        setWaitingForTx(false);
-      })
-      .catch((err) => {
-        setWaitingForTx(false);
-        console.log(err);
-      });
-  }, [callback, contractAddress]);
+  const allow = useCallback(() => {
+    authorizeCurrency({
+      callback,
+      contractAddress,
+      setTxHash,
+      setWaitingForTx,
+    });
+  }, [callback, contractAddress, setTxHash, setWaitingForTx]);
 
   const buttonText = useMemo(() => `Authorize ${symbol}`, [symbol]);
 
