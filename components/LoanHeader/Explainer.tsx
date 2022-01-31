382e6e02cc062b808d6964d17a438e1deb1db62a
Christopher Nascone <chris@nasc.one>
2022-01-31 16:00:59 -0500

    feat: new explainers for loan page

---
+++ components/LoanHeader/Explainer.tsx
@@ -0,0 +1,214 @@
+import { Explainer as ExplainerWrapper } from 'components/Explainer';
+import { LoanFormData } from 'components/LoanForm/LoanFormData';
+import { ethers } from 'ethers';
+import {
+  INTEREST_RATE_PERCENT_DECIMALS,
+  SCALAR,
+  SECONDS_IN_A_YEAR,
+} from 'lib/constants';
+import { daysToSecondsBigNum, secondsBigNumToDays } from 'lib/duration';
+import { formattedAnnualRate } from 'lib/interest';
+import React, { useCallback } from 'react';
+import { FieldError, UseFormReturn } from 'react-hook-form';
+import { Loan } from 'types/Loan';
+
+type ExplainerProps = {
+  form: UseFormReturn<LoanFormData, object>;
+  loan: Loan;
+  state: string;
+  top: number;
+};
+
+type InnerProps = { context: LoanFormData; loan: Loan };
+
+const explainers: {
+  [key: string]: (props: InnerProps) => JSX.Element;
+} = {
+  NoWallet,
+  BetterTermsUnfocused,
+  BetterLoanAmount,
+  BetterDuration,
+  BetterInterestRate,
+  AuthorizeDai,
+  Lend,
+  LendPending,
+  LendSuccess,
+  LendTermsUnfocused,
+  Terms,
+  RepayLoan,
+  SeizeNFT,
+};
+
+export function Explainer({ form, loan, state, top }: ExplainerProps) {
+  const error = Object.values(form.formState.errors)[0];
+  const Inner = explainers[state];
+
+  return (
+    <ExplainerWrapper top={top} display={!!error ? 'error' : 'normal'}>
+      {!!error && <Error error={error} />}
+      {!error && <Inner context={form.watch()} loan={loan} />}
+    </ExplainerWrapper>
+  );
+}
+
+function Error({ error }: { error: FieldError }) {
+  return (
+    <>
+      🚧
+      <div role="alert">{error.message}</div>
+    </>
+  );
+}
+
+function NoWallet({ context }: InnerProps) {
+  return (
+    <div>
+      First, connect a wallet. Then, follow these steps to create a loan and
+      make it available to lenders.
+    </div>
+  );
+}
+
+function BetterTermsUnfocused({ context }: InnerProps) {
+  return (
+    <div>
+      You can become the lender on this loan by offing better terms. Improve one
+      term by at least 10% — a higher loan amount, a lower interest rate, or a
+      longer duration. Click on one to edit.
+    </div>
+  );
+}
+
+function LendTermsUnfocused({ context }: InnerProps) {
+  return (
+    <div>
+      Meet the terms below to become the Lender on this loan. Your funds will be
+      transferred, interest will begin accruing immediately, and you will
+      receive a Lender Ticket (NFT) representing your position.
+    </div>
+  );
+}
+
+function BetterLoanAmount({ loan }: InnerProps) {
+  const minimumImprovement = loan.loanAmount.mul(1.1);
+  const formatAmount = useCallback(
+    (amount: ethers.BigNumber) => {
+      return ethers.utils.formatUnits(amount, loan.loanAssetDecimals);
+    },
+    [loan.loanAssetDecimals],
+  );
+  return (
+    <div>
+      The current loan amount is {formatAmount(loan.loanAmount)}. To improve by
+      10%, lend a higher amount, at least {formatAmount(minimumImprovement)}.
+    </div>
+  );
+}
+
+function BetterDuration({ loan }: InnerProps) {
+  const minimumImprovement = loan.durationSeconds.mul(1.1);
+  return (
+    <div>
+      The current duration is {secondsBigNumToDays(loan.durationSeconds)} days.
+      To improve by 10%, lend for a longer duration, at least{' '}
+      {secondsBigNumToDays(minimumImprovement)} days.
+    </div>
+  );
+}
+
+function BetterInterestRate({
+  context: { duration, interestRate, loanAmount },
+  loan,
+}: InnerProps) {
+  const interestRatePerSecond = interestRate
+    ? ethers.BigNumber.from(
+        Math.floor(
+          parseFloat(interestRate) * 10 ** INTEREST_RATE_PERCENT_DECIMALS,
+        ),
+      ).div(SECONDS_IN_A_YEAR)
+    : loan.perSecondInterestRate;
+  const durationSeconds = duration
+    ? daysToSecondsBigNum(parseFloat(duration))
+    : loan.durationSeconds;
+  const parsedLoanAmount = loanAmount
+    ? ethers.utils.parseUnits(loanAmount.toString(), loan.loanAssetDecimals)
+    : loan.loanAmount;
+  const interestOverTerm = interestRatePerSecond
+    .mul(durationSeconds)
+    .mul(parsedLoanAmount)
+    .div(SCALAR);
+  const estimatedRepayment = ethers.utils.formatUnits(
+    interestOverTerm.add(parsedLoanAmount),
+    loan.loanAssetDecimals,
+  );
+
+  return (
+    <div>
+      Effective rate: <b>{formattedAnnualRate(interestRatePerSecond)}% APR</b>
+      <br />
+      The contract stores and calculates interest on a per-second basis instead
+      of per-year, so actual APR differs slightly from what you input.
+      <br />
+      The estimated repayment at maturity will be{' '}
+      <b>
+        {estimatedRepayment} {loan.loanAssetSymbol}.
+      </b>
+    </div>
+  );
+}
+
+function AuthorizeDai({}: InnerProps) {
+  return (
+    <div>
+      Allow the Pawn Shop to move your DAI. Funds will not be moved until you
+      click &quot;Lend.&quot;
+    </div>
+  );
+}
+
+function Lend({}: InnerProps) {
+  return (
+    <div>
+      Before you deposit funds, take a minute to verify that this NFT is legit.
+      Check its collection and trading history on <b>OpenSea</b> to make sure
+      it&apos;s not a fraud.
+    </div>
+  );
+}
+
+function LendPending({}: InnerProps) {
+  return <div>This can take a few minutes.</div>;
+}
+
+function LendSuccess({}: InnerProps) {
+  return <div>You are now the lender on this loan!</div>;
+}
+
+function Terms() {
+  return (
+    <div>
+      You can edit this detail to make the terms better for the borrower, but
+      you don&apos;t need to.
+    </div>
+  );
+}
+
+function RepayLoan() {
+  return (
+    <div>
+      ”Repay and claim” will pay this amount, close the loan, and transfer the
+      collateral to your wallet.
+    </div>
+  );
+}
+
+function SeizeNFT() {
+  return (
+    <div>
+      Because the Borrower has not repaid the loan, as the Lender you have the
+      opportunity to seize the collateral NFT. If you choose to wait, the
+      Borrower could still repay, and another Lender could still buy you out.
+      Seizing the NFT will close the loan.
+    </div>
+  );
+}
