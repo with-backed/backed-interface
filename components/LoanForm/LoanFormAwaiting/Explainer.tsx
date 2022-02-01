4fd511eb4d9873c140d16a5396ecb65da0650c9b
Christopher Nascone <chris@nasc.one>
2022-02-01 12:14:13 -0500

    feat: loan form awaiting bubbles

--- components/LoanForm/LoanFormAwaiting/Explainer.tsx
+++ components/LoanForm/LoanFormAwaiting/Explainer.tsx
@@ -2,36 +2,33 @@ import { Explainer as ExplainerWrapper } from 'components/Explainer';
 import { LoanFormData } from 'components/LoanForm/LoanFormData';
 import React from 'react';
 import { FieldError, UseFormReturn } from 'react-hook-form';
-import { Loan } from 'types/Loan';
 
 type ExplainerProps = {
   form: UseFormReturn<LoanFormData, object>;
-  loan: Loan;
   state: string;
   top: number;
 };
 
-type InnerProps = { context: LoanFormData; loan: Loan };
-
 const explainers: {
-  [key: string]: (props: InnerProps) => JSX.Element;
+  [key: string]: () => JSX.Element;
 } = {
-  AuthorizeDai,
   Lend,
   LendPending,
   LendSuccess,
   LendTermsUnfocused,
-  Terms,
+  loanAmount: Terms,
+  duration: Terms,
+  interestRate: Terms,
 };
 
-export function Explainer({ form, loan, state, top }: ExplainerProps) {
+export function Explainer({ form, state, top }: ExplainerProps) {
   const error = Object.values(form.formState.errors)[0];
   const Inner = explainers[state];
 
   return (
     <ExplainerWrapper top={top} display={!!error ? 'error' : 'normal'}>
       {!!error && <Error error={error} />}
-      {!error && <Inner context={form.watch()} loan={loan} />}
+      {!error && <Inner />}
     </ExplainerWrapper>
   );
 }
@@ -45,7 +42,7 @@ function Error({ error }: { error: FieldError }) {
   );
 }
 
-function LendTermsUnfocused({ context }: InnerProps) {
+function LendTermsUnfocused() {
   return (
     <div>
       Meet the terms below to become the Lender on this loan. Your funds will be
@@ -55,16 +52,7 @@ function LendTermsUnfocused({ context }: InnerProps) {
   );
 }
 
-function AuthorizeDai({}: InnerProps) {
-  return (
-    <div>
-      Allow the Pawn Shop to move your DAI. Funds will not be moved until you
-      click &quot;Lend.&quot;
-    </div>
-  );
-}
-
-function Lend({}: InnerProps) {
+function Lend() {
   return (
     <div>
       Before you deposit funds, take a minute to verify that this NFT is legit.
@@ -74,11 +62,11 @@ function Lend({}: InnerProps) {
   );
 }
 
-function LendPending({}: InnerProps) {
+function LendPending() {
   return <div>This can take a few minutes.</div>;
 }
 
-function LendSuccess({}: InnerProps) {
+function LendSuccess() {
   return <div>You are now the lender on this loan!</div>;
 }
 
