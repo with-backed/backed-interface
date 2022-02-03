105579bb78a576c4e2975b8152e1949df63dae8d
Christopher Nascone <chris@nasc.one>
2022-02-03 13:29:25 -0500

    fix: can do static image imports
    the config previously here was for an older version of next, and it
    interfered with the way Next 12 handles it

--- next.config.js
+++ next.config.js
@@ -3,18 +3,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 module.exports = withBundleAnalyzer({
   webpack(config) {
-    config.module.rules.push({
-      test: /\.(png|svg)$/,
-      use: {
-        loader: 'url-loader',
-        options: {
-          limit: 8192,
-          publicPath: '/_next/static/',
-          outputPath: 'static/',
-          name: '[name].[ext]',
-        },
-      },
-    });
     return config;
   },
 });
