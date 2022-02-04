6c1803a36ca91c31d89384f021f894c001755fb1
Adam Gobran <adamgobes@gmail.com>
2022-02-04 11:31:20 -0500

    stories

--- stories/LoanInfo.stories.tsx
+++ stories/LoanInfo.stories.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { LoanInfo } from 'components/LoanInfo';
 import { baseLoan, loanWithLenderAccruing } from 'lib/mockData';
 import { CollateralSaleInfo } from 'lib/loans/collateralSaleInfo';
-import { getFakeFloor } from 'lib/nftPort';
+import {
+  getFakeFloor,
+  getFakeItemsAndOwners,
+  getFakeVolume,
+} from 'lib/nftPort';
 import { generateFakeSaleForNFT } from 'lib/nftSalesSubgraph';
 
 export default {
@@ -10,12 +14,20 @@ export default {
   component: LoanInfo,
 };
 
+const [items, owners] = getFakeItemsAndOwners();
+const collectionStats = {
+  floor: getFakeFloor(),
+  items,
+  owners,
+  volume: getFakeVolume(),
+};
+
 const saleInfo: CollateralSaleInfo = {
   recentSale: generateFakeSaleForNFT(
     '0x9ec7ff8964afba6d8c43dc340a2e6c6c3156e966',
     '10',
   ),
-  floorPrice: getFakeFloor(),
+  collectionStats,
 };
 
 export function LoanInfoNoLender() {
