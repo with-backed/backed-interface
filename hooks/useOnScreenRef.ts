7e4deb723aac7f743e73120e59086a1e41a0e65e
Christopher Nascone <cnasc@users.noreply.github.com>
2022-02-04 10:16:09 -0500

    fix: runtime error in useOnScreenRef (#373)
    * fix: runtime error in useOnScreenRef
    
    * types

--- hooks/useOnScreenRef.ts
+++ hooks/useOnScreenRef.ts
@@ -1,10 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, RefObject } from 'react';
 
 // Hook
-export function useOnScreen(ref: any, rootMargin = '0px') {
+export function useOnScreen(ref: RefObject<Element>, rootMargin = '0px') {
   // State and setter for storing whether element is visible
   const [isIntersecting, setIntersecting] = useState(false);
   useEffect(() => {
+    const current = ref.current;
     const observer = new IntersectionObserver(
       ([entry]) => {
         // Update our state when observer callback fires
@@ -14,12 +15,14 @@ export function useOnScreen(ref: any, rootMargin = '0px') {
         rootMargin,
       },
     );
-    if (ref.current) {
-      observer.observe(ref.current);
+    if (current) {
+      observer.observe(current);
     }
     return () => {
-      observer.unobserve(ref.current);
+      if (current) {
+        observer.unobserve(current);
+      }
     };
-  }, []); // Empty array ensures that effect is only run on mount and unmount
+  }, [ref, rootMargin]);
   return isIntersecting;
 }
