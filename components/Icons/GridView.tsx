62e8591a76b1ce7eda71f940ea0832141678db75
Christopher Nascone <chris@nasc.one>
2022-02-08 16:19:37 -0500

    fix: stroke and fill on these

--- components/Icons/GridView.tsx
+++ components/Icons/GridView.tsx
@@ -2,19 +2,7 @@ import * as React from 'react';
 
 export const GridView = () => (
   <svg width={22} height={22} fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect
-      x={1}
-      y={1}
-      width={20}
-      height={20}
-      rx={3}
-      stroke="#2E3A5C"
-      strokeWidth={2}
-    />
-    <path
-      d="M11 1.831v18.333M.916 11h20.167"
-      stroke="#2E3A5C"
-      strokeWidth={2}
-    />
+    <rect x={1} y={1} width={20} height={20} rx={3} strokeWidth={2} />
+    <path d="M11 1.831v18.333M.916 11h20.167" strokeWidth={2} />
   </svg>
 );
