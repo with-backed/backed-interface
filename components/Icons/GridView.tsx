d268db7bab9fba5d3291ccc4184cf3dc9c84b448
Christopher Nascone <chris@nasc.one>
2022-02-08 15:41:00 -0500

    chore: add icons

---
+++ components/Icons/GridView.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+export const GridView = () => (
+  <svg width={22} height={22} fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect
+      x={1}
+      y={1}
+      width={20}
+      height={20}
+      rx={3}
+      stroke="#2E3A5C"
+      strokeWidth={2}
+    />
+    <path
+      d="M11 1.831v18.333M.916 11h20.167"
+      stroke="#2E3A5C"
+      strokeWidth={2}
+    />
+  </svg>
+);
