435b70c69c1106901bd1d54b7d9dfd569657d447
Christopher Nascone <chris@nasc.one>
2022-02-08 16:20:36 -0500

    feat: homepage has a list view option

--- components/AdvancedSearch/Header.tsx
+++ components/AdvancedSearch/Header.tsx
@@ -1,14 +1,17 @@
 import { Button } from 'components/Button';
+import { GridListSelector } from 'components/GridListSelector';
 import styles from './AdvancedSearch.module.css';
 import SortDropdown, { SortOptionValue } from './SortDropdown';
 
 type SearchHeaderProps = {
+  handleViewChange: (checked: boolean) => void;
   setSelectedSort: (sort: SortOptionValue) => void;
   showSearch: boolean;
   setShowSearch: (showSearch: boolean) => void;
 };
 
 export function SearchHeader({
+  handleViewChange,
   setSelectedSort,
   showSearch,
   setShowSearch,
@@ -16,6 +19,7 @@ export function SearchHeader({
   return (
     <div className={styles.header}>
       <div className={styles.searchButton}>
+        <GridListSelector handleChange={handleViewChange} />
         <Button
           kind={`${showSearch ? 'secondary' : 'primary-dark-bg'}`}
           onClick={() => setShowSearch(!showSearch)}>
--- components/HomePageLoans/HomePageLoans.tsx
+++ components/HomePageLoans/HomePageLoans.tsx
@@ -1,4 +1,3 @@
-import { TwelveColumn } from 'components/layouts/TwelveColumn';
 import { LoanCard } from 'components/LoanCard';
 import { LoanTable } from 'components/LoanTable';
 import React from 'react';
@@ -12,11 +11,11 @@ type HomePageLoansProps = {
 export function HomePageLoans({ loans, view }: HomePageLoansProps) {
   if (view === 'cards') {
     return (
-      <TwelveColumn>
+      <>
         {loans.map((l) => (
           <LoanCard key={l.id.toString()} loan={l} />
         ))}
-      </TwelveColumn>
+      </>
     );
   }
 
