be03c3716c19d2c814e2315f83af0255b76bd2be
Christopher Nascone <chris@nasc.one>
2022-02-08 13:07:07 -0500

    chore: deprecate TwoColumn

--- components/layouts/TwoColumn/TwoColumn.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FunctionComponent } from 'react';
-
-import styles from './TwoColumn.module.css';
-
-export const TwoColumn: FunctionComponent = ({ children }) => (
-  <div className={styles.grid}>{children}</div>
-);
