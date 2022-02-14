5a20d7915c23e0321d9eeea777ffcfd9e82a77d8
Christopher Nascone <cnasc@users.noreply.github.com>
2022-02-14 17:05:27 -0500

    feat: fallback images shimmer (#404)
    * feat: fallback images shimmer
    
    * fix: animation is optional
    
    * fix: test
    
    * chore: minimize console noise in tests

--- jest.setup.js
+++ jest.setup.js
@@ -2,4 +2,7 @@ import path from 'path';
 import dontenv from 'dotenv';
 import '@testing-library/jest-dom/extend-expect';
 
+// Minimize console output when testing failure cases
+global.console.error = jest.fn();
+
 dontenv.config({ path: path.resolve(__dirname, './.env.test') });
