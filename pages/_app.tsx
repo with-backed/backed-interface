a9cab41924c5219f0d9e02f0d49cc910d9ea5cdc
Christopher Nascone <chris@nasc.one>
2022-02-17 16:19:23 -0500

    fix: various misc fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import { Web3Provider } from '@ethersproject/providers';
 import { PawnShopHeader } from 'components/PawnShopHeader';
 import { AppWrapper } from 'components/layouts/AppWrapper';
 import { TimestampProvider } from 'hooks/useTimestamp/useTimestamp';
+import { useEffect, useState } from 'react';
 
 const getLibrary: React.ComponentProps<typeof Web3ReactProvider>['getLibrary'] =
   (provider) => {
@@ -15,12 +16,46 @@ const getLibrary: React.ComponentProps<typeof Web3ReactProvider>['getLibrary'] =
     return library;
   };
 
+const konami = [
+  'ArrowUp',
+  'ArrowUp',
+  'ArrowDown',
+  'ArrowDown',
+  'ArrowLeft',
+  'ArrowRight',
+  'ArrowLeft',
+  'ArrowRight',
+  'KeyB',
+  'KeyA',
+  'Enter',
+];
+let konamiIndex = 0;
+
 export default function App({ Component, pageProps }: AppProps) {
+  const [showVariant, setShowVariant] = useState(false);
+  useEffect(() => {
+    function handleKeyDown(ev: KeyboardEvent) {
+      if (ev.code === konami[konamiIndex]) {
+        ++konamiIndex;
+      } else {
+        konamiIndex = 0;
+      }
+
+      if (konamiIndex >= konami.length) {
+        konamiIndex = 0;
+        setShowVariant((prev) => !prev);
+      }
+    }
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => document.removeEventListener('keydown', handleKeyDown);
+  }, []);
+
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
       <TimestampProvider>
         <AppWrapper>
-          <PawnShopHeader />
+          <PawnShopHeader prawn={showVariant} />
           <Component {...pageProps} />
         </AppWrapper>
       </TimestampProvider>
