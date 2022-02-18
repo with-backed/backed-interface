07654ad36837f9ce6641c29e464f2c26c64dcb85
Christopher Nascone <chris@nasc.one>
2022-02-18 11:06:00 -0500

    refactor: include id in events for reverse lookup

--- pages/profile/[address].tsx
+++ pages/profile/[address].tsx
@@ -1,6 +1,9 @@
 import { GetServerSideProps } from 'next';
 import React, { useMemo } from 'react';
-import { getAllActiveLoansForAddress } from 'lib/loans/subgraph/getAllLoansEventsForAddress';
+import {
+  getAllActiveLoansForAddress,
+  getAllEventsForAddress,
+} from 'lib/loans/subgraph/getAllLoansEventsForAddress';
 import { Loan as RawSubgraphLoan } from 'types/generated/graphql/nftLoans';
 import { ProfileHeader } from 'components/Profile/ProfileHeader';
 import { parseSubgraphLoan } from 'lib/loans/utils';
@@ -20,6 +23,9 @@ export const getServerSideProps: GetServerSideProps<ProfilePageProps> = async (
   const address = (await resolveEns(rawAddress)) || rawAddress;
 
   const allLoansForAddress = await getAllActiveLoansForAddress(address);
+  const events = await getAllEventsForAddress(address);
+
+  console.dir({ events }, { depth: null });
 
   return {
     props: {
