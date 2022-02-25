7f236390a6dad10a71fcd5472e9b94a279aa87ee
Christopher Nascone <chris@nasc.one>
2022-02-25 11:20:32 -0500

    fix: redo activity layout

--- components/ProfileActivity/ProfileActivity.tsx
+++ components/ProfileActivity/ProfileActivity.tsx
@@ -1,10 +1,7 @@
 import { EtherscanTransactionLink } from 'components/EtherscanLink';
 import { TwelveColumn } from 'components/layouts/TwelveColumn';
 import { NFTMedia } from 'components/Media/NFTMedia';
-import { ethers } from 'ethers';
 import { useTokenMetadata } from 'hooks/useTokenMetadata';
-import { secondsBigNumToDays } from 'lib/duration';
-import { formattedAnnualRate } from 'lib/interest';
 import { renderEventName } from 'lib/text';
 import Link from 'next/link';
 import React, { useMemo } from 'react';
@@ -41,12 +38,10 @@ export function ProfileActivity({ events, loans }: ProfileActivityProps) {
       <table className={styles.table}>
         <thead>
           <tr>
-            <th></th>
+            <th>User</th>
             <th>Event</th>
+            <th>Time</th>
             <th>Collateral</th>
-            <th className={styles.right}>Amount</th>
-            <th className={styles.right}>Duration</th>
-            <th className={styles.right}>Rate</th>
           </tr>
         </thead>
         <tbody>
@@ -58,23 +53,13 @@ export function ProfileActivity({ events, loans }: ProfileActivityProps) {
                   <Relationship typename={e.typename} />
                 </td>
                 <td>
-                  <EventLink
-                    id={e.id}
-                    typename={e.typename}
-                    timestamp={e.timestamp}
-                  />
+                  <EventLink id={e.id} typename={e.typename} />
+                </td>
+                <td>
+                  <EventDate timestamp={e.timestamp} />
                 </td>
                 <td>
                   <EventCollateral loan={loan} />
-                </td>
-                <td className={styles.right}>
-                  <EventAmount event={e} loan={loan} />
-                </td>
-                <td className={styles.right}>
-                  <EventDuration event={e} loan={loan} />
-                </td>
-                <td className={styles.right}>
-                  <EventRate event={e} loan={loan} />
                 </td>
               </tr>
             );
@@ -94,18 +79,22 @@ function Relationship({ typename }: RelationshipProps) {
   return <span className={styles[relationship]}>{relationship}</span>;
 }
 
-type EventLinkProps = Pick<Event, 'typename' | 'id' | 'timestamp'>;
-function EventLink({ id, timestamp, typename }: EventLinkProps) {
+type EventDateProps = Pick<Event, 'timestamp'>;
+function EventDate({ timestamp }: EventDateProps) {
   const date = useMemo(
     () => new Date(timestamp * 1000).toLocaleString(),
     [timestamp],
   );
+  return <span>{date}</span>;
+}
+
+type EventLinkProps = Pick<Event, 'typename' | 'id'>;
+function EventLink({ id, typename }: EventLinkProps) {
   return (
     <div className={styles['event-link']}>
       <EtherscanTransactionLink transactionHash={id}>
         {renderEventName(typename)} 🔗
       </EtherscanTransactionLink>
-      {date}
     </div>
   );
 }
@@ -140,61 +129,4 @@ function EventCollateral({ loan }: EventCollateralProps) {
       </a>
     </Link>
   );
-}
-
-type EventDetailProps = {
-  event: Event;
-  loan: Loan;
-};
-
-function EventAmount({ event, loan }: EventDetailProps) {
-  const amount = useMemo(() => {
-    let amount = loan.loanAmount;
-    if (event.typename === 'BuyoutEvent') {
-      amount = event.replacedAmount;
-    }
-    if (event.typename === 'CreateEvent') {
-      amount = event.minLoanAmount;
-    }
-    if (event.typename === 'LendEvent' || event.typename === 'RepaymentEvent') {
-      amount = event.loanAmount;
-    }
-
-    return ethers.utils.formatUnits(amount, loan.loanAssetDecimals);
-  }, [event, loan]);
-  return (
-    <span>
-      {amount} {loan.loanAssetSymbol}
-    </span>
-  );
-}
-
-function EventDuration({ event, loan }: EventDetailProps) {
-  const duration = useMemo(() => {
-    let duration = loan.durationSeconds;
-    if (event.typename === 'CreateEvent') {
-      duration = event.minDurationSeconds;
-    }
-    if (event.typename === 'LendEvent') {
-      duration = event.durationSeconds;
-    }
-
-    return secondsBigNumToDays(duration);
-  }, [event, loan]);
-  return <span>{duration} days</span>;
-}
-
-function EventRate({ event, loan }: EventDetailProps) {
-  const rate = useMemo(() => {
-    let rate = loan.perSecondInterestRate;
-    if (event.typename === 'CreateEvent') {
-      rate = event.maxInterestRate;
-    }
-    if (event.typename === 'LendEvent') {
-      rate = event.interestRate;
-    }
-
-    return formattedAnnualRate(rate);
-  }, [event, loan]);
-  return <span>{rate} %</span>;
 }
