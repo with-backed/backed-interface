57a26476c32b0aed5be54ed07fd4adab7806ac88
Christopher Nascone <chris@nasc.one>
2022-02-28 15:47:42 -0500

    chore: opt in to SWC minification

--- next.config.js
+++ next.config.js
@@ -2,7 +2,5 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 module.exports = withBundleAnalyzer({
-  webpack(config) {
-    return config;
-  },
+  swcMinify: true,
 });
