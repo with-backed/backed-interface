4cbddb4f238d1b8569ccae1827b744bed4458511
Christopher Nascone <chris@nasc.one>
2022-02-28 15:27:16 -0500

    feat: banners essentially work

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Banner.module.css';
 
-export function Banner() {
-  return <div></div>;
+export type BannerKind = 'error' | 'success' | 'info';
+
+type BannerProps = {
+  kind: BannerKind;
+  close?: () => void;
+};
+
+export function Banner({
+  children,
+  close,
+  kind,
+}: React.PropsWithChildren<BannerProps>) {
+  return (
+    <div className={styles[kind]}>
+      <div className={styles.inner}>{children}</div>
+      <button
+        aria-label="close message"
+        className={styles.close}
+        onClick={close}>
+        ×
+      </button>
+    </div>
+  );
 }
--- components/Banner/index.ts
+++ components/Banner/index.ts
@@ -1,1 +1,2 @@
 export { Banner } from './Banner';
+export type { BannerKind } from './Banner';
