364f6b0a37eb3513796a9793edbbf8f66bbed9e9
Christopher Nascone <cnasc@users.noreply.github.com>
2022-03-01 09:15:09 -0500

    [NFT-95] chore: upgrade to Next 12.1 (#458)
    * chore: upgrade next
    
    * chore: opt in to SWC minification
    
    * chore: use next/jest
    
    * chore: boost Input coverage
    
    * chore: boost coverage on Media
    
    * chore: remove data-testid

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
-module.exports = {
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
   collectCoverageFrom: [
     'components/**/*.{js,jsx,ts,tsx}',
     'hooks/**/*.{js,jsx,ts,tsx}',
@@ -17,33 +23,9 @@ module.exports = {
     },
   },
   moduleDirectories: ['node_modules', '<rootDir>'],
-  moduleNameMapper: {
-    /* Handle CSS imports (with CSS modules)
-    https://jestjs.io/docs/webpack#mocking-css-modules */
-    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
-
-    // Handle CSS imports (without CSS modules)
-    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
-
-    /* Handle image imports
-    https://jestjs.io/docs/webpack#handling-static-assets */
-    '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$':
-      '<rootDir>/__mocks__/fileMock.js',
-  },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testPathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/.next/',
-    '<rootDir>/pages/',
-  ],
   testEnvironment: 'jsdom',
-  transform: {
-    /* Use babel-jest to transpile tests with the next/babel preset
-    https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
-  },
-  transformIgnorePatterns: [
-    '/node_modules/',
-    '^.+\\.module\\.(css|sass|scss)$',
-  ],
+  testPathIgnorePatterns: ['<rootDir>/pages/'],
 };
+
+module.exports = createJestConfig(customJestConfig);
--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 module.exports = withBundleAnalyzer({
-  webpack(config) {
-    return config;
+  swcMinify: true,
+  compiler: {
+    // remove properties matching the default regex ^data-test
+    reactRemoveProperties: true,
   },
 });
