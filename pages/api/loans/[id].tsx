8fa164105c4044ce4b5a774677a72b793c6a6f15
Christopher Nascone <chris@nasc.one>
2022-03-01 14:49:23 -0500

    feat: example use of generation

--- pages/api/loans/[id].tsx
+++ pages/api/loans/[id].tsx
@@ -1,21 +1,18 @@
 import { subgraphLoanById } from 'lib/loans/subgraph/subgraphLoanById';
-import { Loan as SubgraphLoan } from 'types/generated/graphql/nftLoans';
+import { LoanByIdQuery } from 'types/generated/graphql/graphql-operations';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 // TODO: Should probably not just relying on
 // the subgraph, but fall back to the node, if the call didn't work
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<SubgraphLoan | null>,
+  res: NextApiResponse<LoanByIdQuery['loan'] | null>,
 ) {
   try {
-    const { id, includeEvents } = req.query;
+    const { id } = req.query;
     const idString: string = Array.isArray(id) ? id[0] : id;
-    const includeEventsBool: boolean = Array.isArray(includeEvents)
-      ? includeEvents[0] == 'true'
-      : includeEvents == 'true';
 
-    const loan = await subgraphLoanById(idString, includeEventsBool);
+    const loan = await subgraphLoanById(idString);
     res.status(200).json(loan);
   } catch (e) {
     // TODO: bugsnag
