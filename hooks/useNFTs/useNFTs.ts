5e23d52c9afb47dcf0414f967a474b40ea1bd21d
Christopher Nascone <chris@nasc.one>
2022-03-03 14:49:16 -0500

    feat: type eip721 subgraph query

--- hooks/useNFTs/useNFTs.ts
+++ hooks/useNFTs/useNFTs.ts
@@ -1,45 +1,14 @@
 import { ethers } from 'ethers';
+import {
+  NftsQuery,
+  NftsDocument,
+} from 'types/generated/graphql/eip721subgraph';
 import { NFTEntity } from 'types/NFT';
 import { useQuery } from 'urql';
 
-type RawNFT = Omit<NFTEntity, 'id'> & { id: string };
-
-type Data =
-  | {
-      account: {
-        id: string;
-        tokens: RawNFT[];
-      };
-    }
-  | undefined
-  | null;
-
-const NFTsQuery = `
-query NFTs($address: String!) {
-  account(id: $address) {
-    id
-    tokens {
-      id
-      identifier
-      uri
-      registry {
-          symbol
-          name
-      }
-      approvals {
-        id
-        approved {
-          id
-        }
-      }
-    }
-  }
-}
-`;
-
 export const useNFTs = (address: string) => {
-  const [result] = useQuery<Data>({
-    query: NFTsQuery,
+  const [result] = useQuery<NftsQuery>({
+    query: NftsDocument,
     variables: {
       address: address.toLowerCase(),
     },
