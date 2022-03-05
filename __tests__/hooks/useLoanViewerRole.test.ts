3a0a661247baf9169c4caf2e31fa922daf9accfa
Caleb Carithers <calebcarithers@me.com>
2022-03-05 21:46:01 +0000

    tests for neither lender nor borrower

--- __tests__/hooks/useLoanViewerRole.test.ts
+++ __tests__/hooks/useLoanViewerRole.test.ts
@@ -4,6 +4,7 @@ import { baseLoan, loanWithLenderAccruing } from 'lib/mockData';
 
 const borrower = '0x143b357C6f9E029991A7eE10259B3a888191e408';
 const lender = '0x141B3B7862E2C2e3410039d9fe7F2617E98E7992';
+const neitherLenderNorBorrower = '0x6acB8F94DD966fd28014305387093B4b3252A00f';
 
 describe('useLoanViewerRole', () => {
   it('renders borrower role', () => {
@@ -32,6 +33,23 @@ describe('useLoanViewerRole', () => {
 
   it('renders null for null lender', () => {
     const result = renderHook(() => useLoanViewerRole(baseLoan, lender));
+    expect(result.result.current).toEqual(null);
+  });
+
+  it('renders null for account that is neither the lender nor borrower for a loan with a lender', () => {
+    const result = renderHook(() =>
+      useLoanViewerRole(
+        { ...loanWithLenderAccruing, lender },
+        neitherLenderNorBorrower,
+      ),
+    );
+    expect(result.result.current).toEqual(null);
+  });
+
+  it('renders null for account that is neither the lender nor borrower for a loan with no lender', () => {
+    const result = renderHook(() =>
+      useLoanViewerRole(baseLoan, neitherLenderNorBorrower),
+    );
     expect(result.result.current).toEqual(null);
   });
 });
