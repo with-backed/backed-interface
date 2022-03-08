c056d0ce84393f4a95ee102b7089f749e3a0e96e
Adam Gobran <adamgobes@gmail.com>
2022-03-08 18:22:52 -0500

    try to skip consumers middleware

--- pages/api/events/consumers/_middleware.ts
+++ pages/api/events/consumers/_middleware.ts
@@ -1,18 +1,19 @@
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
 export default function middleware(req: NextRequest, _ev?: NextFetchEvent) {
-  try {
-    const isSubscriptionConfirmationRequest = 'SubscribeURL' in req.body!;
-    if (!isSubscriptionConfirmationRequest) {
-      return NextResponse.next();
-    } else {
-      fetch(req.body['SubscribeURL'], {
-        method: 'GET',
-      });
-      return new NextResponse(undefined, { status: 200 });
-    }
-  } catch (e) {
-    console.error(e);
-    return new NextResponse(undefined, { status: 500 });
-  }
+  return NextResponse.next();
+  // try {
+  //     const isSubscriptionConfirmationRequest = 'SubscribeURL' in req.body!;
+  //     if (!isSubscriptionConfirmationRequest) {
+  //         return NextResponse.next();
+  //     } else {
+  //         fetch(req.body['SubscribeURL'], {
+  //             method: 'GET',
+  //         });
+  //         return new NextResponse(undefined, { status: 200 });
+  //     }
+  // } catch (e) {
+  //     console.error(e);
+  //     return new NextResponse(undefined, { status: 500 });
+  // }
 }
