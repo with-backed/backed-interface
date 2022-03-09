79e55e592794df0d97cae033312ca70700522553
Christopher Nascone <chris@nasc.one>
2022-03-09 13:06:37 -0500

    chore: mark bugsnag points on eventsHelpers

--- lib/eventsHelpers.ts
+++ lib/eventsHelpers.ts
@@ -2,14 +2,10 @@ import { nftBackedLoansClient } from './urql';
 import {
   BuyoutByTransactionHashDocument,
   BuyoutByTransactionHashQuery,
-  BuyoutEvent,
-  CollateralSeizureEvent,
   CollateralSeizureEventByTransactionHashDocument,
   CollateralSeizureEventByTransactionHashQuery,
   LendByTransactionHashDocument,
   LendByTransactionHashQuery,
-  LendEvent,
-  RepaymentEvent,
   RepaymentEventByTransactionHashDocument,
   RepaymentEventByTransactionHashQuery,
 } from 'types/generated/graphql/nftLoans';
@@ -21,34 +17,55 @@ export async function subgraphEventFromTxHash(
 ): Promise<RawSubgraphEvent | undefined | null> {
   switch (eventName) {
     case 'BuyoutEvent':
-      const { data: buyoutEventData } = await nftBackedLoansClient
-        .query<BuyoutByTransactionHashQuery>(BuyoutByTransactionHashDocument, {
-          id: txHash,
-        })
-        .toPromise();
+      const { data: buyoutEventData, error: buyoutEventError } =
+        await nftBackedLoansClient
+          .query<BuyoutByTransactionHashQuery>(
+            BuyoutByTransactionHashDocument,
+            {
+              id: txHash,
+            },
+          )
+          .toPromise();
+      if (buyoutEventError) {
+        // TODO: bugsnag
+      }
       return buyoutEventData?.buyoutEvent;
     case 'LendEvent':
-      const { data: lendEventData } = await nftBackedLoansClient
-        .query<LendByTransactionHashQuery>(LendByTransactionHashDocument, {
-          id: txHash,
-        })
-        .toPromise();
+      const { data: lendEventData, error: lendEventError } =
+        await nftBackedLoansClient
+          .query<LendByTransactionHashQuery>(LendByTransactionHashDocument, {
+            id: txHash,
+          })
+          .toPromise();
+      if (lendEventError) {
+        // TODO: bugsnag
+      }
       return lendEventData?.lendEvent;
     case 'RepaymentEvent':
-      const { data: repayEventData } = await nftBackedLoansClient
-        .query<RepaymentEventByTransactionHashQuery>(
-          RepaymentEventByTransactionHashDocument,
-          { id: txHash },
-        )
-        .toPromise();
+      const { data: repayEventData, error: repayEventError } =
+        await nftBackedLoansClient
+          .query<RepaymentEventByTransactionHashQuery>(
+            RepaymentEventByTransactionHashDocument,
+            { id: txHash },
+          )
+          .toPromise();
+      if (repayEventError) {
+        // TODO: bugsnag
+      }
       return repayEventData?.repaymentEvent;
     case 'CollateralSeizureEvent':
-      const { data: collateralSeizureEventData } = await nftBackedLoansClient
+      const {
+        data: collateralSeizureEventData,
+        error: collateralSeizureEventError,
+      } = await nftBackedLoansClient
         .query<CollateralSeizureEventByTransactionHashQuery>(
           CollateralSeizureEventByTransactionHashDocument,
           { id: txHash },
         )
         .toPromise();
+      if (collateralSeizureEventError) {
+        // TODO: bugsnag
+      }
       return collateralSeizureEventData?.collateralSeizureEvent;
     default:
       return null;
