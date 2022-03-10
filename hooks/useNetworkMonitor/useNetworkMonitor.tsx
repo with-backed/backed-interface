9bc87086b24e74cd1f71722e2c5556c8da8af219
Christopher Nascone <chris@nasc.one>
2022-03-10 14:25:22 -0500

    feat: clear banner when chainId changes

--- hooks/useNetworkMonitor/useNetworkMonitor.tsx
+++ hooks/useNetworkMonitor/useNetworkMonitor.tsx
@@ -1,17 +1,22 @@
 import { WrongNetwork } from 'components/Banner/messages';
-import { useGlobalMessages } from 'hooks/useGlobalMessages';
+import { useGlobalMessages, Message } from 'hooks/useGlobalMessages';
 import { useWeb3 } from 'hooks/useWeb3';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const expectedChainId = parseInt(process.env.NEXT_PUBLIC_CHAIN_ID as string);
 
 export function useNetworkMonitor() {
+  const [currentMessage, setCurrentMessage] = useState<Message | null>(null);
   const { chainId } = useWeb3();
-  const { addMessage } = useGlobalMessages();
+  const { addMessage, removeMessage } = useGlobalMessages();
+
   useEffect(() => {
+    if (currentMessage) {
+      removeMessage(currentMessage);
+    }
     if (chainId) {
       if (chainId !== expectedChainId) {
-        addMessage({
+        const message: Message = {
           kind: 'error',
           message: (
             <WrongNetwork
@@ -19,8 +24,10 @@ export function useNetworkMonitor() {
               expectedChainId={expectedChainId}
             />
           ),
-        });
+        };
+        addMessage(message);
+        setCurrentMessage(message);
       }
     }
-  }, [addMessage, chainId]);
+  }, [addMessage, chainId, currentMessage, removeMessage]);
 }
