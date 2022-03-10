a89947656983a1e99d5b5e5c4aade87fff783c45
Adam Gobran <adamgobes@gmail.com>
2022-03-10 10:54:31 -0500

    nft-135: modify liquidation cron to run every hour

--- lib/events/timely/timely.ts
+++ lib/events/timely/timely.ts
@@ -5,6 +5,8 @@ import {
   overrideLastWrittenTimestamp,
 } from '../consumers/userNotifications/repository';
 
+const HOURS_IN_DAY = 24;
+
 type LiquidatedLoans = {
   liquidationOccurringLoans: Loan[];
   liquidationOccurredLoans: Loan[];
@@ -13,7 +15,11 @@ type LiquidatedLoans = {
 export async function getLiquidatedLoansForTimestamp(
   currentTimestamp: number,
 ): Promise<LiquidatedLoans> {
-  if (process.env.NEXT_PUBLIC_NOTIFICATIONS_KILLSWITCH)
+  const notificationsFreq = parseInt(
+    process.env.NEXT_PUBLIC_NOTIFICATIONS_FREQUENCY_HOURS!,
+  );
+
+  if (process.env.NEXT_PUBLIC_NOTIFICATIONS_KILLSWITCH || !notificationsFreq)
     return {
       liquidationOccurredLoans: [],
       liquidationOccurringLoans: [],
@@ -22,23 +28,21 @@ export async function getLiquidatedLoansForTimestamp(
   let pastTimestamp = await getLastWrittenTimestamp();
   if (!pastTimestamp) {
     // if we couldn't get pastTimestamp from postgres for whatever reason, just default to N hours before
-    pastTimestamp =
-      currentTimestamp -
-      parseInt(process.env.NEXT_PUBLIC_NOTIFICATIONS_FREQUENCY_HOURS!) * 3600;
+    pastTimestamp = currentTimestamp - notificationsFreq * 3600;
   }
 
-  const futureTimestamp =
-    currentTimestamp +
-    parseInt(process.env.NEXT_PUBLIC_NOTIFICATIONS_FREQUENCY_HOURS!) * 3600;
+  // ensure no timestamps get missed by using the last run timestamp + 1 hour (or whatever of freq is)
+  // in a perfect world, currentTimestamp === currentRunTimestamp, but with inconsistincies in our cron or anything else, they may differ
+  const currentRunTimestamp = pastTimestamp + notificationsFreq * 3600;
 
   const liquidationOccurringLoans = await getLoansExpiringWithin(
-    currentTimestamp,
-    futureTimestamp,
+    currentRunTimestamp + (HOURS_IN_DAY + notificationsFreq) * 3600,
+    currentRunTimestamp + HOURS_IN_DAY * 3600,
   );
 
   const liquidationOccurredLoans = await getLoansExpiringWithin(
-    pastTimestamp,
-    currentTimestamp,
+    currentRunTimestamp - notificationsFreq * 3600,
+    currentRunTimestamp,
   );
 
   await overrideLastWrittenTimestamp(currentTimestamp);
