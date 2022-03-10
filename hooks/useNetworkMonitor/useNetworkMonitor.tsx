c59b60c0fc8cb88a4eacf6749ff42c3abebc8e27
Christopher Nascone <chris@nasc.one>
2022-03-10 14:50:51 -0500

    fix: hook infinite loop

--- hooks/useNetworkMonitor/useNetworkMonitor.tsx
+++ hooks/useNetworkMonitor/useNetworkMonitor.tsx
@@ -6,14 +6,11 @@ import React, { useEffect, useState } from 'react';
 const expectedChainId = parseInt(process.env.NEXT_PUBLIC_CHAIN_ID as string);
 
 export function useNetworkMonitor() {
-  const [currentMessage, setCurrentMessage] = useState<Message | null>(null);
+  const [currentMessages, setCurrentMessages] = useState<Message[]>([]);
   const { chainId } = useWeb3();
   const { addMessage, removeMessage } = useGlobalMessages();
 
   useEffect(() => {
-    if (currentMessage) {
-      removeMessage(currentMessage);
-    }
     if (chainId) {
       if (chainId !== expectedChainId) {
         const message: Message = {
@@ -26,8 +23,17 @@ export function useNetworkMonitor() {
           ),
         };
         addMessage(message);
-        setCurrentMessage(message);
+        setCurrentMessages((prev) => [...prev, message]);
       }
     }
-  }, [addMessage, chainId, currentMessage, removeMessage]);
+  }, [addMessage, chainId]);
+
+  useEffect(() => {
+    if (chainId) {
+      if (chainId === expectedChainId && currentMessages.length > 0) {
+        currentMessages.forEach((m) => removeMessage(m));
+        setCurrentMessages([]);
+      }
+    }
+  }, [chainId, currentMessages, removeMessage, setCurrentMessages]);
 }
