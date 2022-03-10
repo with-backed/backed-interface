687631c698679fd934bdfdf25956ba40d57c8ad4
Christopher Nascone <chris@nasc.one>
2022-03-10 15:00:27 -0500

    feat: banner tries to switch network

--- components/Banner/messages/WrongNetwork.tsx
+++ components/Banner/messages/WrongNetwork.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import { useGlobalMessages } from 'hooks/useGlobalMessages';
+import { useWeb3 } from 'hooks/useWeb3';
+import React, { useCallback } from 'react';
 
 const idToName: { [key: number]: string } = {
   1: 'Mainnet',
@@ -15,11 +17,30 @@ export const WrongNetwork = ({
   currentChainId,
   expectedChainId,
 }: WrongNetworkProps) => {
+  const { library } = useWeb3();
+  const { addMessage } = useGlobalMessages();
+
+  const handleClick = useCallback(async () => {
+    try {
+      await library!.jsonRpcFetchFunc('wallet_switchEthereumChain', [
+        { chainId: `0x${expectedChainId.toString(16)}` },
+      ]);
+    } catch (e) {
+      addMessage({
+        kind: 'error',
+        message:
+          'Looks like your wallet does not support automatic network changes. Please change the network manually.',
+      });
+    }
+  }, [addMessage, expectedChainId, library]);
+
   return (
     <span>
       You&apos;re viewing data from the {idToName[expectedChainId]} network, but
       your wallet is connected to the {idToName[currentChainId]} network.{' '}
-      <button>Switch to {idToName[expectedChainId]}</button>
+      <button onClick={handleClick}>
+        Switch to {idToName[expectedChainId]}
+      </button>
     </span>
   );
 };
