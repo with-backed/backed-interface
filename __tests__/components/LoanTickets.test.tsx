aa5841fb0106a85f8e9e78947bbb996f9186dd25
Adam Gobran <adamgobes@gmail.com>
2022-03-22 12:54:28 -0400

    loan ticket test

--- __tests__/components/LoanTickets.test.tsx
+++ __tests__/components/LoanTickets.test.tsx
@@ -1,24 +1,42 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { LoanTickets } from 'components/LoanTickets';
 import { baseLoan, loanWithLenderAccruing } from 'lib/mockData';
+import { addressToENS } from 'lib/account';
 
 jest.mock('components/Media', () => ({
   ...jest.requireActual('components/Media'),
   Media: () => <div></div>,
 }));
 
+jest.mock('lib/account', () => ({
+  ...jest.requireActual('lib/account'),
+  addressToENS: jest.fn(),
+}));
+
+const mockAddressToENS = addressToENS as jest.MockedFunction<
+  typeof addressToENS
+>;
+
 describe('LoanTickets', () => {
-  it('renders tickets for a loan with no lender', () => {
+  beforeEach(() => {
+    mockAddressToENS.mockResolvedValue(null);
+  });
+  it('renders tickets for a loan with no lender', async () => {
     const { getByText } = render(<LoanTickets loan={baseLoan} />);
+    await screen.findByText(baseLoan.borrower);
+
     // borrower address
     getByText(baseLoan.borrower);
 
     getByText('No lender yet');
   });
 
-  it('renders tickets for a loan with a lender', () => {
+  it('renders tickets for a loan with a lender', async () => {
     const { getByText } = render(<LoanTickets loan={loanWithLenderAccruing} />);
+    await screen.findByText(loanWithLenderAccruing.borrower);
+    await screen.findByText(loanWithLenderAccruing.lender as string);
+
     // borrower address
     getByText(loanWithLenderAccruing.borrower);
 
