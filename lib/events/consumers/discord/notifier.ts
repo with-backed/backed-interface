df2f371400a98d997611a76f56eb041522940dc3
Adam Gobran <adamgobes@gmail.com>
2022-03-25 16:12:39 -0400

    logging for discord

--- lib/events/consumers/discord/notifier.ts
+++ lib/events/consumers/discord/notifier.ts
@@ -4,6 +4,13 @@ export async function sendBotMessage(content: string) {
   const client = new Discord.Client();
   await client.login(process.env.DISCORD_BOT_TOKEN!);
 
+  console.log({
+    isUndefined: !!client.channels.cache.get(
+      process.env.NEXT_PUBLIC_BACKED_UPDATES_CHANNEL_ID!,
+    ),
+    name: client.user?.tag || 'undefined name',
+  });
+
   await (
     client.channels.cache.get(
       process.env.NEXT_PUBLIC_BACKED_UPDATES_CHANNEL_ID!,
