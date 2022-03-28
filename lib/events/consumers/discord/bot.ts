2e300d6d8bcd3cc69c3fd627adac9070211d6ef5
Adam Gobran <adamgobes@gmail.com>
2022-03-28 12:07:53 -0400

    create by tx hash

--- lib/events/consumers/discord/bot.ts
+++ lib/events/consumers/discord/bot.ts
@@ -4,8 +4,8 @@ export async function sendBotMessage(content: string) {
   const client = new Discord.Client();
   await client.login(process.env.DISCORD_BOT_TOKEN!);
 
-  const channel = (await client.channels.cache.find(
-    (c) => c.id === process.env.NEXT_PUBLIC_BACKED_UPDATES_CHANNEL_ID!,
+  const channel = (await client.channels.fetch(
+    process.env.NEXT_PUBLIC_BACKED_UPDATES_CHANNEL_ID!,
   )) as Discord.TextChannel;
 
   console.log({
