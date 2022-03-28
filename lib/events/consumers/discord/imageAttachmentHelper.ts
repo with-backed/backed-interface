f5378f5002c7ece3b13234cd077411e55dbabb31
Adam Gobran <adamgobes@gmail.com>
2022-03-28 17:39:28 -0400

    json parse

--- lib/events/consumers/discord/imageAttachmentHelper.ts
+++ lib/events/consumers/discord/imageAttachmentHelper.ts
@@ -4,22 +4,42 @@ import { NFTResponseData } from 'pages/api/nftInfo/[uri]';
 import { svg2png } from 'svg-png-converter';
 
 const SVG_PREFIX = 'data:image/svg+xml;base64,';
+const JSON_PREFIX = 'data:application/json;base64,';
 
 export async function collateralToDiscordMessageEmbed(
   collateralName: string,
   collateralTokenId: ethers.BigNumber,
   collateralTokenURI: string,
 ): Promise<MessageEmbed> {
+  let NFTInfo: NFTResponseData;
+
   const isDataUri = collateralTokenURI.startsWith('data:');
-  const tokenURIRes = await fetch(
-    isDataUri
-      ? collateralTokenURI
-      : `https://rinkeby.withbacked.xyz/api/nftInfo/${encodeURIComponent(
-          collateralTokenURI,
-        )}`,
-  );
-  console.log({ tokenURIRes });
-  const NFTInfo: NFTResponseData = await tokenURIRes.json();
+
+  if (isDataUri) {
+    console.log({
+      sub: collateralTokenURI.substring(
+        collateralTokenURI.indexOf(JSON_PREFIX) + 2,
+      ),
+    });
+    NFTInfo = JSON.parse(
+      Buffer.from(
+        collateralTokenURI.substring(
+          collateralTokenURI.indexOf(JSON_PREFIX) + 2,
+        ),
+        'base64',
+      ).toString(),
+    );
+  } else {
+    const tokenURIRes = await fetch(
+      isDataUri
+        ? collateralTokenURI
+        : `https://rinkeby.withbacked.xyz/api/nftInfo/${encodeURIComponent(
+            collateralTokenURI,
+          )}`,
+    );
+    NFTInfo = await tokenURIRes.json();
+  }
+
   console.log({ NFTInfo });
 
   if (NFTInfo!.image.startsWith(SVG_PREFIX)) {
