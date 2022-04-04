986f88b81a8c8285e22b454549eb1d4ada288086
Adam Gobran <adamgobes@gmail.com>
2022-04-04 17:26:53 -0400

    Revert "NFT-192: DB setup for discord metrics (#544)"
    This reverts commit 29814c2c0d26576bced4e5d2d149773321b88e58.

--- prisma/migrations/20220401180609_discord_metrics/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "DiscordMetrics" (
-    "id" SERIAL NOT NULL,
-    "numLoansCreated" INTEGER NOT NULL,
-    "numLoansLentTo" INTEGER NOT NULL,
-    "dollarLoansLentTo" INTEGER NOT NULL,
-
-    CONSTRAINT "DiscordMetrics_pkey" PRIMARY KEY ("id")
-);
