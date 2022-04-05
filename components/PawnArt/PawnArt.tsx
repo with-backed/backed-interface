21f11da5344d7676d0d5142a32a7f942c8438b0b
Christopher Nascone <chris@nasc.one>
2022-04-05 16:36:25 -0400

    fix: loan tickets do not rerender

--- components/PawnArt/PawnArt.tsx
+++ components/PawnArt/PawnArt.tsx
@@ -1,47 +1,57 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { ethers } from 'ethers';
-import { getNFTInfo, GetNFTInfoResponse } from 'lib/getNFTInfo';
+import { GetNFTInfoResponse } from 'lib/getNFTInfo';
 import { Media } from 'components/Media';
 import { ERC721 } from 'types/generated/abis';
 import { jsonRpcERC721Contract } from 'lib/contracts';
 import { Fallback } from 'components/Media/Fallback';
+import { useTokenMetadata } from 'hooks/useTokenMetadata';
 
 interface PawnArtProps {
   contract: ERC721;
-  tokenId: ethers.BigNumber;
+  tokenID: ethers.BigNumber;
 }
 
-export function PawnLoanArt({ tokenId }: Pick<PawnArtProps, 'tokenId'>) {
-  const pawnLoansContract = jsonRpcERC721Contract(
-    process.env.NEXT_PUBLIC_LEND_TICKET_CONTRACT || '',
+export const PawnLoanArt = React.memo(
+  ({ tokenID }: Pick<PawnArtProps, 'tokenID'>) => {
+    const pawnLoansContract = jsonRpcERC721Contract(
+      process.env.NEXT_PUBLIC_LEND_TICKET_CONTRACT || '',
+    );
+    return <PawnArt contract={pawnLoansContract} tokenID={tokenID} />;
+  },
+);
+PawnLoanArt.displayName = 'PawnLoanArt';
+
+export const PawnTicketArt = React.memo(
+  ({ tokenID }: Pick<PawnArtProps, 'tokenID'>) => {
+    const pawnTicketsContract = jsonRpcERC721Contract(
+      process.env.NEXT_PUBLIC_BORROW_TICKET_CONTRACT || '',
+    );
+    return <PawnArt contract={pawnTicketsContract} tokenID={tokenID} />;
+  },
+);
+PawnTicketArt.displayName = 'PawnTicketArt';
+
+function PawnArt({ contract, tokenID }: PawnArtProps) {
+  const [tokenURI, setTokenURI] = useState('');
+  const tokenSpec = useMemo(
+    () => ({
+      tokenURI,
+      tokenID,
+    }),
+    [tokenID, tokenURI],
   );
-  return <PawnArt contract={pawnLoansContract} tokenId={tokenId} />;
-}
-
-export function PawnTicketArt({ tokenId }: Pick<PawnArtProps, 'tokenId'>) {
-  const pawnTicketsContract = jsonRpcERC721Contract(
-    process.env.NEXT_PUBLIC_BORROW_TICKET_CONTRACT || '',
-  );
-  return <PawnArt contract={pawnTicketsContract} tokenId={tokenId} />;
-}
-
-function PawnArt({ contract, tokenId }: PawnArtProps) {
-  const [nftInfo, setNFTInfo] = useState<GetNFTInfoResponse | null>(null);
-
-  const load = useCallback(async () => {
-    const result = await getNFTInfo({ contract, tokenId });
-    setNFTInfo(result);
-  }, [contract, tokenId]);
+  const { isLoading, metadata } = useTokenMetadata(tokenSpec);
 
   useEffect(() => {
-    load();
-  }, [load]);
+    contract.tokenURI(tokenID).then(setTokenURI);
+  }, [contract, tokenID]);
 
-  if (nftInfo === null) {
+  if (isLoading || !metadata) {
     return <Fallback />;
   }
 
-  return <PawnArtLoaded {...nftInfo} />;
+  return <PawnArtLoaded {...metadata} />;
 }
 
 function PawnArtLoaded(nftInfo: GetNFTInfoResponse) {
