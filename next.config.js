635ee66636dff403d1eebb4c48e6ea46eeafe1f8
Christopher Nascone <chris@nasc.one>
2022-04-08 12:06:25 -0400

    config: install and configure bugsnag

--- next.config.js
+++ next.config.js
@@ -7,4 +7,6 @@ module.exports = withBundleAnalyzer({
     // remove properties matching the default regex ^data-test
     reactRemoveProperties: true,
   },
+  // TODO: figure out CI to upload source maps to Bugsnag and then enable
+  //productionBrowserSourceMaps: true,
 });
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,14 @@ import { providers } from 'ethers';
 import { TimestampProvider } from 'hooks/useTimestamp/useTimestamp';
 import { GlobalMessagingProvider } from 'hooks/useGlobalMessages';
 import { WagmiProvider, chain } from 'wagmi';
+import Bugsnag from '@bugsnag/js';
+import BugsnagPluginReact from '@bugsnag/plugin-react';
+import React from 'react';
+
+Bugsnag.start({
+  apiKey: '14558dbf2fab438a64debb70104edc45',
+  plugins: [new BugsnagPluginReact()],
+});
 
 const jsonRpcProvider = new providers.JsonRpcProvider(
   process.env.NEXT_PUBLIC_JSON_RPC_PROVIDER,
@@ -35,22 +43,26 @@ const connectors = connectorsForWallets(wallets)({
   chainId: parseInt(process.env.NEXT_PUBLIC_CHAIN_ID || '1'),
 });
 
+const ErrorBoundary = Bugsnag.getPlugin('react')!.createErrorBoundary(React);
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <GlobalMessagingProvider>
-      <RainbowKitProvider theme={lightTheme()} chains={chains}>
-        <WagmiProvider
-          autoConnect
-          connectors={connectors}
-          provider={jsonRpcProvider}>
-          <TimestampProvider>
-            <AppWrapper>
-              <PawnShopHeader />
-              <Component {...pageProps} />
-            </AppWrapper>
-          </TimestampProvider>
-        </WagmiProvider>
-      </RainbowKitProvider>
-    </GlobalMessagingProvider>
+    <ErrorBoundary>
+      <GlobalMessagingProvider>
+        <RainbowKitProvider theme={lightTheme()} chains={chains}>
+          <WagmiProvider
+            autoConnect
+            connectors={connectors}
+            provider={jsonRpcProvider}>
+            <TimestampProvider>
+              <AppWrapper>
+                <PawnShopHeader />
+                <Component {...pageProps} />
+              </AppWrapper>
+            </TimestampProvider>
+          </WagmiProvider>
+        </RainbowKitProvider>
+      </GlobalMessagingProvider>
+    </ErrorBoundary>
   );
 }
