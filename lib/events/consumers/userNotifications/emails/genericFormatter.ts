9ffd2b824a5e1a3d8431467637870edf38517f14
Adam Gobran <adamgobes@gmail.com>
2022-04-14 09:00:59 -0500

    exhaustive switch

--- lib/events/consumers/userNotifications/emails/genericFormatter.ts
+++ lib/events/consumers/userNotifications/emails/genericFormatter.ts
@@ -7,11 +7,15 @@ export type GenericEmailComponents = {
   footer: string;
 };
 
+const throwInvalidGenericEmailType = (_: never): never => {
+  throw new Error('Invalid generic email type passed');
+};
+
 export function getSubjectForGenericEmail(type: GenericEmailType): string {
   switch (type) {
     case GenericEmailType.CONFIRMATION:
       return 'Backed: Email request received';
     default:
-      return '';
+      return throwInvalidGenericEmailType(type);
   }
 }
