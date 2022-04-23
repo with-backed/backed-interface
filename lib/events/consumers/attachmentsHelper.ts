b9700ebd3a98a299fe5582b5201b395dfd90fdb8
Adam Gobran <adamgobes@gmail.com>
2022-04-22 22:41:12 -0400

    env

--- lib/events/consumers/attachmentsHelper.ts
+++ lib/events/consumers/attachmentsHelper.ts
@@ -1,11 +1,8 @@
 import axios from 'axios';
 
 export async function getPngBufferFromBase64SVG(base: string): Promise<string> {
-  const pngBufferRes = await axios.post(
-    `https://svg-to-png-buffer.vercel.app/api/svgToPngBuffer`,
-    {
-      svg: base,
-    },
-  );
+  const pngBufferRes = await axios.post(`${process.env.SVG_TO_PNG_URL!}`, {
+    svg: base,
+  });
   return pngBufferRes.data.pngBuffer;
 }
