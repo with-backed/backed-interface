4a342aff635d8d741549eec05884664449c09bfd
Christopher Nascone <chris@nasc.one>
2022-04-25 13:48:08 -0400

    fix: pass env json rpc url

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,9 +28,7 @@ const chains: Chain[] = [{ ...chain.rinkeby, name: 'Rinkeby' }];
 const wallets = getDefaultWallets({
   chains,
   appName: 'Backed',
-  jsonRpcUrl: ({ chainId }) =>
-    chains.find((x) => x.id === chainId)?.rpcUrls?.[0] ??
-    chain.mainnet.rpcUrls[0],
+  jsonRpcUrl: process.env.NEXT_PUBLIC_JSON_RPC_PROVIDER!,
 });
 
 wallets[0].wallets.sort((a, b) => a.id.localeCompare(b.id));
