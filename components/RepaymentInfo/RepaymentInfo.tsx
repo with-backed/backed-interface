4ba741d52feaa3f613a85a43408e294a021928a0
Adam Gobran <adamgobes@gmail.com>
2022-04-26 17:36:03 -0400

    progress

--- components/RepaymentInfo/RepaymentInfo.tsx
+++ components/RepaymentInfo/RepaymentInfo.tsx
@@ -2,7 +2,10 @@ import { DescriptionList } from 'components/DescriptionList';
 import { Fieldset } from 'components/Fieldset';
 import { useLoanDetails } from 'hooks/useLoanDetails';
 import { Loan } from 'types/Loan';
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
+import dayjs from 'dayjs';
+import { EventAttributes, createEvent } from 'ics';
+import { captureException } from '@sentry/nextjs';
 
 type RepaymentInfoProps = {
   loan: Loan;
@@ -14,6 +17,45 @@ export function RepaymentInfo({ loan }: RepaymentInfoProps) {
     longFormattedInterestRate,
     longFormattedEstimatedPaybackAtMaturity,
   } = useLoanDetails(loan);
+
+  const maturityDate = useMemo(
+    () =>
+      loan.endDateTimestamp
+        ? dayjs.unix(loan.endDateTimestamp).format('MMMM D, YYYY')
+        : '',
+    [loan],
+  );
+
+  const createICSEvent = useCallback(() => {
+    const endDate = dayjs.unix(loan.endDateTimestamp!);
+    const start: [number, number, number] = [
+      endDate.year(),
+      endDate.day(),
+      endDate.month(),
+    ];
+
+    const event: EventAttributes = {
+      start,
+      title: `Loan ${loan.id}: ${loan.collateralName} ${loan.collateralTokenId} due`,
+      description: 'Loan repayment for backed is due',
+      url: `http://www.withbacked.xyz/loans/${loan.id}`,
+      duration: {
+        days: 1,
+      },
+    };
+
+    let returnValue: string = '';
+    createEvent(event, (error, value) => {
+      if (error) {
+        captureException(error);
+      }
+
+      returnValue = value;
+    });
+
+    return returnValue;
+  }, [loan]);
+
   return (
     <Fieldset legend="💰 Repayment">
       <DescriptionList orientation="horizontal">
@@ -23,6 +65,14 @@ export function RepaymentInfo({ loan }: RepaymentInfoProps) {
         <dd>{longFormattedInterestRate}</dd>
         <dt>Payback at maturity</dt>
         <dd>{longFormattedEstimatedPaybackAtMaturity}</dd>
+        <dt>Maturity Date</dt>
+        <dd>
+          {maturityDate}{' '}
+          <img
+            src={'/cal-icon.svg'}
+            onClick={() => window.open(createICSEvent(), '_blank')}
+          />
+        </dd>
       </DescriptionList>
     </Fieldset>
   );
