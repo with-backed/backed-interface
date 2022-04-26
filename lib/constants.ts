3753204664dabe3dc022941340fe3cee0293bc83
Christopher Nascone <cnasc@users.noreply.github.com>
2022-04-26 13:00:04 -0400

    fix: use correct channel name on 404/500 (#638)

--- lib/constants.ts
+++ lib/constants.ts
@@ -10,5 +10,6 @@ export const SCALAR = ethers.BigNumber.from(10).pow(
 );
 
 export const DISCORD_URL = 'https://discord.gg/ZCxGuE6Ytk';
+export const DISCORD_ERROR_CHANNEL = '#🪲bug-reports';
 export const TWITTER_URL = 'https://twitter.com/backed_xyz';
 export const GITHUB_URL = 'https://github.com/with-backed';
