75a7cdcfaabd5c452dbab47f1ea2dbea5112beec
Christopher Nascone <chris@nasc.one>
2022-05-06 12:24:16 -0400

    fix: mock useConfig globally

--- jest.setup.js
+++ jest.setup.js
@@ -3,6 +3,7 @@ import dontenv from 'dotenv';
 import '@testing-library/jest-dom/extend-expect';
 import fetchMock from 'jest-fetch-mock';
 import { TextEncoder, TextDecoder } from 'util';
+import { configs } from './lib/config';
 
 // Minimize console output when testing failure cases
 global.console.error = jest.fn();
@@ -12,3 +13,8 @@ dontenv.config({ path: path.resolve(__dirname, './.env.test') });
 global.fetch = fetchMock;
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
+
+jest.mock('./hooks/useConfig', () => ({
+  ...jest.requireActual('./hooks/useConfig'),
+  useConfig: jest.fn(() => configs.rinkeby),
+}));
