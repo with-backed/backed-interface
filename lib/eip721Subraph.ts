5332e1bb0183f45fbd0d05b0ceba9c6c6e0743ec
Christopher Nascone <chris@nasc.one>
2022-05-10 12:18:12 -0400

    chore: remove unused function

--- lib/eip721Subraph.ts
+++ lib/eip721Subraph.ts
@@ -10,11 +10,6 @@ export function getNftContractAddress(nft: NFTEntity): string {
   return nft.id.substring(0, 42);
 }
 
-export function constructEtherscanLinkForNft(nft: NFTEntity): string {
-  const etherscanUrl = process.env.NEXT_PUBLIC_ETHERSCAN_URL;
-  return `${etherscanUrl}/address/${getNftContractAddress(nft)}`;
-}
-
 export function isNFTApprovedForCollateral(nft: NFTEntity): boolean {
   return (
     nft.approvals.filter(
