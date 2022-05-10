55cdaed5c2386fe635b89d39187443ec490b133c
Christopher Nascone <chris@nasc.one>
2022-05-10 17:33:26 -0400

    fix: coingecko lib more explicit

--- lib/coingecko.ts
+++ lib/coingecko.ts
@@ -5,15 +5,20 @@ export async function getUnitPriceForCoin(
   toCurrency: string,
   network?: SupportedNetwork,
 ): Promise<number | undefined> {
-  if (network !== 'ethereum') {
-    return undefined;
+  if (network === 'rinkeby') {
+    return 1.01;
   }
 
-  const statsRes = await fetch(
-    `https://api.coingecko.com/api/v3/coins/ethereum/contract/${tokenAddress}`,
-  );
+  if (network === 'ethereum') {
+    const statsRes = await fetch(
+      `https://api.coingecko.com/api/v3/coins/ethereum/contract/${tokenAddress}`,
+    );
 
-  const json = await statsRes.json();
+    const json = await statsRes.json();
 
-  return json?.market_data?.current_price[toCurrency];
+    return json?.market_data?.current_price[toCurrency];
+  }
+
+  // unhandled network
+  return undefined;
 }
