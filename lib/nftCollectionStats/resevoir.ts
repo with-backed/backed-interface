0ee1c81fc969dcefa0848e5a0a8ee757372ad22d
Adam Gobran <adamgobes@gmail.com>
2022-05-12 12:32:08 -0400

    custom resevoir handling

--- lib/nftCollectionStats/resevoir.ts
+++ lib/nftCollectionStats/resevoir.ts
@@ -1,7 +1,18 @@
 import { CollectionStatistics } from 'lib/nftCollectionStats';
 
+const ART_BLOCKS_CONTRACT_ADDRESS =
+  '0xa7d8d9ef8D8Ce8992Df33D8b8CF4Aebabd5bD270';
+
+const resevoirResToStats = (json: any): CollectionStatistics => ({
+  floor: json?.collection?.floorAsk.price,
+  items: json?.collection?.tokenCount,
+  owners: json?.collection?.ownerCount,
+  volume: json?.collection?.volume.allTime,
+});
+
 export async function collectionStatsEthMainnet(
   contractAddress: string,
+  tokenId: string,
 ): Promise<CollectionStatistics> {
   const resevoirAssetReq = await fetch(
     `https://api.reservoir.tools/collection/v2?id=${contractAddress}`,
@@ -13,12 +24,35 @@ export async function collectionStatsEthMainnet(
     },
   );
 
-  const json = (await resevoirAssetReq.json()) as any;
+  if (contractAddress === ART_BLOCKS_CONTRACT_ADDRESS) {
+    return handleArtBlocks(tokenId);
+  }
 
-  return {
-    floor: json?.collection?.floorAsk.price,
-    items: json?.collection?.tokenCount,
-    owners: json?.collection?.ownerCount,
-    volume: json?.collection?.volume.allTime,
-  };
+  return resevoirResToStats(await resevoirAssetReq.json());
+}
+
+async function handleArtBlocks(tokenId: string): Promise<CollectionStatistics> {
+  const tokensV4Req = await fetch(
+    `https://api.reservoir.tools/tokens/v4?tokens=${ART_BLOCKS_CONTRACT_ADDRESS}:${tokenId}`,
+    {
+      headers: new Headers({
+        Accept: 'application/json',
+        'x-api-key': process.env.RESEVOIR_API_KEY!,
+      }),
+    },
+  );
+
+  const collectionId = (await tokensV4Req.json()).collection.id;
+
+  const collectionV2Req = await fetch(
+    `https://api.reservoir.tools/collection/v2?id=${collectionId}`,
+    {
+      headers: new Headers({
+        Accept: 'application/json',
+        'x-api-key': process.env.RESEVOIR_API_KEY!,
+      }),
+    },
+  );
+
+  return resevoirResToStats(await collectionV2Req.json());
 }
