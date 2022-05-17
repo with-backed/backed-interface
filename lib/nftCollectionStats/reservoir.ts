44bb7561bf3c9ec34fe3ced372aafd3e343410cc
Adam Gobran <adamgobes@gmail.com>
2022-05-17 09:24:45 -0400

    trivial: make request to tokens v4 for all NFTs with reservoir (#682)
    * trivial: update art block contract address list to support more
    
    * trivial: make request to tokens v4 for all NFTs with reservoir

--- lib/nftCollectionStats/reservoir.ts
+++ lib/nftCollectionStats/reservoir.ts
@@ -1,8 +1,5 @@
 import { CollectionStatistics } from 'lib/nftCollectionStats';
 
-const ART_BLOCKS_CONTRACT_ADDRESS =
-  '0xa7d8d9ef8D8Ce8992Df33D8b8CF4Aebabd5bD270';
-
 const reservoirResToStats = (json: any): CollectionStatistics => ({
   floor: json?.collection?.floorAsk.price || null,
   items: json?.collection?.tokenCount || null,
@@ -14,26 +11,8 @@ export async function collectionStatsEthMainnet(
   contractAddress: string,
   tokenId: string,
 ): Promise<CollectionStatistics> {
-  if (contractAddress === ART_BLOCKS_CONTRACT_ADDRESS) {
-    return handleArtBlocks(tokenId);
-  }
-
-  const reservoirAssetReq = await fetch(
-    `https://api.reservoir.tools/collection/v2?id=${contractAddress}`,
-    {
-      headers: new Headers({
-        Accept: 'application/json',
-        'x-api-key': process.env.RESERVOIR_API_KEY!,
-      }),
-    },
-  );
-
-  return reservoirResToStats(await reservoirAssetReq.json());
-}
-
-async function handleArtBlocks(tokenId: string): Promise<CollectionStatistics> {
   const tokensV4Req = await fetch(
-    `https://api.reservoir.tools/tokens/v4?tokens=${ART_BLOCKS_CONTRACT_ADDRESS}:${tokenId}`,
+    `https://api.reservoir.tools/tokens/v4?tokens=${contractAddress}:${tokenId}`,
     {
       headers: new Headers({
         Accept: 'application/json',
