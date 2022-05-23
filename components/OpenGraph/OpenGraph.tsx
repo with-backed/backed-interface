0fdefcdf32e6a876d9c7f20ada71046125b2ff69
Christopher Nascone <chris@nasc.one>
2022-05-23 16:10:33 -0400

    chore: add twitter-specific tags

--- components/OpenGraph/OpenGraph.tsx
+++ components/OpenGraph/OpenGraph.tsx
@@ -20,16 +20,22 @@ export function OpenGraph({
   return (
     <Head>
       <meta property="og:title" content={title} />
+      <meta property="twitter:title" content={title} />
       <title>{title}</title>
       <meta property="og:type" content={type} />
       <meta property="og:url" content={siteUrl + pathname} />
+      <meta property="twitter:url" content={siteUrl + pathname} />
       <meta property="og:image" content={imageUrl} />
+      <meta property="twitter:image" content={imageUrl} />
       {!!description && (
         <>
           <meta property="og:description" content={description} />
+          <meta name="twitter:description" content={description} />
           <meta name="description" content={description} />
         </>
       )}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="backed_xyz" />
     </Head>
   );
 }
