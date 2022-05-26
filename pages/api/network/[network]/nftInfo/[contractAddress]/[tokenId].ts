d4905a231af424dfa9cef236a74f57b465d4b5fe
Christopher Nascone <chris@nasc.one>
2022-05-26 15:45:02 -0400

    fix: try-catches in nftInfo api

--- pages/api/network/[network]/nftInfo/[contractAddress]/[tokenId].ts
+++ pages/api/network/[network]/nftInfo/[contractAddress]/[tokenId].ts
@@ -27,17 +27,7 @@ async function handler(
 
     return res.status(200).json(metadata);
   } catch (e) {
-    if (e instanceof Error) {
-      if (
-        e.name === 'FetchError' &&
-        e.message.startsWith('invalid json response body')
-      ) {
-        // this happens when the root CID exists but the child file does not. No way to get the file, do nothing.
-      } else {
-        captureException(e);
-      }
-    }
-
+    captureException(e);
     // TODO: we could respond with a failure reason and surface that in the UI.
     return res.status(404).json(null);
   }
@@ -55,35 +45,61 @@ export async function getMetadata(
   contractAddress: string,
   tokenId: ethers.BigNumber,
 ): Promise<NFTResponseData> {
-  const { jsonRpcProvider } = configs[network];
-  const contract = jsonRpcERC721Contract(contractAddress, jsonRpcProvider);
-  const uri = await contract.tokenURI(ethers.BigNumber.from(tokenId));
-  const resolvedUri = convertIPFS(uri);
-  if (!resolvedUri) {
-    throw new Error(`Could not resolve ${uri}`);
+  try {
+    const { jsonRpcProvider } = configs[network];
+    const contract = jsonRpcERC721Contract(contractAddress, jsonRpcProvider);
+    const uri = await contract.tokenURI(ethers.BigNumber.from(tokenId));
+    const resolvedUri = convertIPFS(uri);
+    if (!resolvedUri) {
+      throw new Error(`Could not resolve ${uri}`);
+    }
+
+    const json = await getJson(resolvedUri);
+
+    if (!json) {
+      throw new Error(`Could not fetch json from ${uri}`);
+    }
+
+    const { name, description, image, image_url, animation_url, external_url } =
+      json;
+
+    const media = await getMedia({ animation_url, image, image_url });
+
+    return {
+      name,
+      description,
+      tokenId: tokenId.toNumber(),
+      ...media,
+      external_url,
+    };
+  } catch (e) {
+    captureException(e);
+    return null;
   }
-
-  const { name, description, image, image_url, animation_url, external_url } =
-    await getJson(resolvedUri);
-
-  const media = await getMedia({ animation_url, image, image_url });
-
-  return {
-    name,
-    description,
-    tokenId: tokenId.toNumber(),
-    ...media,
-    external_url,
-  };
 }
 
 async function getJson(uri: string) {
-  if (uri.startsWith(DATA_URI_PREFIX)) {
-    // TODO: not always base64? not always json?
-    const buffer = Buffer.from(uri.split(',')[1], 'base64');
-    return JSON.parse(buffer.toString());
-  } else {
-    const res = await fetch(uri);
-    return await res.json();
+  try {
+    if (uri.startsWith(DATA_URI_PREFIX)) {
+      // TODO: not always base64? not always json?
+      const buffer = Buffer.from(uri.split(',')[1], 'base64');
+      return JSON.parse(buffer.toString());
+    } else {
+      const res = await fetch(uri);
+      return await res.json();
+    }
+  } catch (e) {
+    if (e instanceof Error) {
+      if (
+        e.name === 'FetchError' &&
+        e.message.startsWith('invalid json response body')
+      ) {
+        // this happens when the root CID exists but the child file does not. No way to get the file, do nothing.
+      } else {
+        captureException(e);
+      }
+    }
+
+    return null;
   }
 }
