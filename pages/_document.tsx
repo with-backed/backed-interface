93c0b169b71970a86426daaf12347f37f4531107
Christopher Nascone <chris@nasc.one>
2022-06-16 09:32:32 -0400

    fix: set crossorigin on font preload

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,7 @@ function FontLinks() {
           href={`${FONT_URL_PREFIX}/${f}.woff`}
           as="font"
           type="font/woff"
+          crossOrigin="anonymous"
         />
       ))}
     </>
