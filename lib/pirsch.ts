acdeefb1f0752565749b9bccb0e347e02a355ca4
Christopher Nascone <chris@nasc.one>
2022-06-28 12:25:09 -0400

    docs

--- lib/pirsch.ts
+++ lib/pirsch.ts
@@ -1,4 +1,5 @@
 export const pirsch: typeof window.pirsch = (...args) => {
+  // Pirsch may be blocked by extensions; only try to send if it really exists
   if (window.pirsch) {
     window.pirsch(...args);
   }
