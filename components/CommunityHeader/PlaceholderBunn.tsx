b352ba5698c106d702efb45f84857c9467d9358a
Christopher Nascone <chris@nasc.one>
2022-07-06 10:35:20 -0400

    more cleanup

--- components/CommunityHeader/PlaceholderBunn.tsx
+++ components/CommunityHeader/PlaceholderBunn.tsx
@@ -1,4 +1,3 @@
 import * as React from 'react';
 import placeholderBunn from './placeholder-bunn.svg';
-console.log({ placeholderBunn });
 export const PlaceholderBunn = () => <img src={placeholderBunn.src} alt="" />;
